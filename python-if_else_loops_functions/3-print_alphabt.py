42685ed7992ee0e046f0216831fb003243945dfa
Vitushan <vitushan@outlook.fr>
2025-01-16 10:23:19 +0100

    fixing pycodestyle

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(letter)) for letter in range(97, 123)\
- if letter not in (101, 113)), end="")
+print("".join("{}".format(chr(letter)) for letter in range(97, 123) if letter not in (101, 113)), end="")
