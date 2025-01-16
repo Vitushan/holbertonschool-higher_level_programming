3c41a4b50069a3497ffdb764215fb098bbf124b8
Vitushan <vitushan@outlook.fr>
2025-01-16 07:27:44 +0100

    display decimal and hexadecimal

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i != 8 or j != 9:
-            print("{:01d}{:01d},,".format(i, j), end=" ")
+            print("{:01d}{:01d},".format(i, j), end=" ")
         else:
             print("{:01d}{:01d}".format( i, j))
