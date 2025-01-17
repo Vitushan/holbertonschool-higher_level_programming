d9caf1a703e2a74e761ad7938f5f33d1b1415a63
Vitushan <vitushan@outlook.fr>
2025-01-17 14:25:51 +0100

    argc argv

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
       print("0 arguments.")
    elif argc == 1:
       print("1 aruments:")
-      print("1 {}".format(sys.argv[1]))
+      print("1: {}".format(sys.argv[1]))
    else:
       print("{} arguments:".format(argc))
       for i in range(1, argc + 1):
-       print("{}: {}.".format(i, sys.argv[i]))
+       print("{}: {}".format(i, sys.argv[i]))
