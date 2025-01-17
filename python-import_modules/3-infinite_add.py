5ccd08468a35dd6976c69d2a6c36c731242579d0
Vitushan <vitushan@outlook.fr>
2025-01-17 16:30:43 +0100

    argc argv

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argc= len(sys.argv)
+    argc = len(sys.argv)
     result = 0
     for i in range(1, argc):
         result += (int)(sys.argv[i])
