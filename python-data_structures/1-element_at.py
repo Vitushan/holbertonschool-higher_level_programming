dc75a6355425921e8361f986e5ee5a01eb99a808
Vitushan <vitushan@outlook.fr>
2025-01-20 10:45:05 +0100

    print integer number lists

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """
+    Récupère un élément d'une liste à un indice donné.
+
+    :param my_list: La liste dans laquelle récupérer l'élément
+    :param idx: L'indice de l'élément à récupérer
+    :return: L'élément à l'indice spécifié, ou None si l'indice est invalide
+    """
     if idx < 0:
         return None
 
     if idx >= len(my_list):
         return None
+
+    return my_list[idx]
