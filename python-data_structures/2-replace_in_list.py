79edc90b551e9926a4ab39257015929a19846cd6
Vitushan <vitushan@outlook.fr>
2025-01-20 11:01:15 +0100

    docstring

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """
-    fonction qui replace la liste
+    fonction replace_in_list remplace la liste
+    en parametre idx et element.
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
