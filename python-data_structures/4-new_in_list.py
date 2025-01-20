381af233397b0db17ee7e720dc1419445ad0a0a7
Vitushan <vitushan@outlook.fr>
2025-01-20 15:25:17 +0100

    replace element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -9,5 +9,5 @@ def new_in_list(my_list, idx, element):
     my_cpylist = my_list[:]
     my_cpylist[idx] = element
     return (my_cpylist)
-if idx < 0 or idx >= len(my_list):
-    return (my_list[:])
+    if idx < 0 or idx >= len(my_list):
+     return (my_list[:])
