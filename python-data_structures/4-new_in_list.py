251914e8c2159cd2bfb1d6fb78c967df509807b5
Vitushan <vitushan@outlook.fr>
2025-01-20 15:32:00 +0100

    replace element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -6,8 +6,8 @@ def new_in_list(my_list, idx, element):
     cet fonction remplace un élément dans une liste à une position
     spécifique sans modifier la liste d'origine.
     """
-    my_cpylist = my_list[:]
-    my_cpylist[idx] = element
-    return (my_cpylist)
-    if idx < 0 or idx >= len(my_list):
-     return (my_list[:])
+    if 0 <= idx < len(my_list):
+        my_cpylist = my_list[:]
+        my_cpylist[idx] = element
+        return (my_cpylist)
+    return (my_list)
