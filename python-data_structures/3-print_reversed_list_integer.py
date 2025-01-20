ce820ff98038ece58f42d0df9a7824c4e9710365
Vitushan <vitushan@outlook.fr>
2025-01-20 14:43:16 +0100

    reversePrint

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-   reverse_num = my_list[::-1]
-   for num in reverse_num:
-      print("{:d}".format(num))
+    """
+    print_reversed_list_integer est une fonction
+    qui prend en parametre:my_list
+    ceci est une fonction qui imprimes les entier a l'envers.
+    """
+    reverse_num = my_list[::-1]
+    for num in reverse_num:
+        print("{:d}".format(num))
