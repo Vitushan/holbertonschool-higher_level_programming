7b490b51c40df990a9192a20f02fe46b70b8635a
Vitushan <vitushan@outlook.fr>
2025-01-20 15:24:27 +0100

    replace element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,5 +7,7 @@ def new_in_list(my_list, idx, element):
     spécifique sans modifier la liste d'origine.
     """
     my_cpylist = my_list[:]
-if idx < 0 or idx >= len(my_list):
+    my_cpylist[idx] = element
     return (my_cpylist)
+if idx < 0 or idx >= len(my_list):
+    return (my_list[:])
