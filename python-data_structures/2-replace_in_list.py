387dc4328814489471a4a4e35a68861ed969738d
Vitushan <vitushan@outlook.fr>
2025-01-20 10:55:06 +0100

    print integer number lists

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,7 +3,6 @@ def replace_in_list(my_list, idx, element):
     """
     fonction qui replace la liste
     """
-    if idx < 0:
+    if idx < 0 or idx >= len(my_list):
         return my_list
-    if idx >= len(my_list):
-        return my_list
+
