028d9938a3f068021d51ddf5a53b61ac7876b967
Vitushan <vitushan@outlook.fr>
2025-01-20 22:00:43 +0100

    sentence

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        return (None, 0)
     else:
-        return len(sentence)
+        return (len(sentence), sentence[0])
