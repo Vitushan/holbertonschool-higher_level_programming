a119b7f87e23ba954990165d227ab158db4b3eac
Vitushan <vitushan@outlook.fr>
2025-01-21 20:38:08 +0100

    sort keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
-        print(key)
+        print(f"{} {}".format(key, a_dictionary[key]))
