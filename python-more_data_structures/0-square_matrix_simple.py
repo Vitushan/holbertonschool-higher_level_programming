f2281a370c9e4ff55595537be6b02794386aa51a
Vitushan <vitushan@outlook.fr>
2025-01-21 16:16:04 +0100

    new directory

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    
+    new_matrix = []
+    for i in matrix:
+        new_line = []
+        for j in i:
+            new_line.append(j**2)
+        new_matrix.append(new_line)   
+    return new_matrix
