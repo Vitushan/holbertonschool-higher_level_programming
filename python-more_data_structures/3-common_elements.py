ba3aa36004e18211cf241dc7431ed60beaf96c50
Vitushan <vitushan@outlook.fr>
2025-01-21 20:03:10 +0100

    keys

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+L'opérateur & est utilisé pour trouver l'intersection entre deux ensembles.
+Cela renvoie un nouvel ensemble contenant uniquement
+les éléments communs aux deux ensembles.
+"""
 def common_elements(set_1, set_2):
     return set_1 & set_2
--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+L'opérateur ^ est utilisé pour trouver la différence symétrique entre deux ensembles.
+Cela renvoie un nouvel ensemble contenant les éléments
+qui sont dans
+un seul des deux ensembles (pas dans les deux à la fois).
+"""
 def only_diff_elements(set_1, set_2):
     return set_1 ^ set_2
