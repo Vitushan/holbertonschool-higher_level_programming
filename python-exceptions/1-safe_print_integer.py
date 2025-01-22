dd2b6d1e171a82f08afc53e6da1ed9affcb9861b
Vitushan <vitushan@outlook.fr>
2025-01-22 21:54:50 +0100

    try / except

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except Except as e:
-        print(type(e))
+    except ValueError:
         return False
