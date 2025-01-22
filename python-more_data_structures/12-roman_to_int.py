b4568b7d0bea36df1f96e55a574a2879bf5fb664
Vitushan <vitushan@outlook.fr>
2025-01-22 14:36:59 +0100

    romane number

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,4 @@ def roman_to_int(roman_string):
     if not roman_string or if isinstance(roman_string, str):
         print(f"{roman_string} is not a string.")
         return 0
-    else:
-        print(f"{roman_string} is a string.")
-    return 1
+
