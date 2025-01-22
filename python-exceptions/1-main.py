8e79d83b10aea12c55c346f8c9604b2ef6a3b3c8
Vitushan <vitushan@outlook.fr>
2025-01-22 22:08:35 +0100

    try / except

--- python-exceptions/1-main.py
+++ python-exceptions/1-main.py
@@ -15,3 +15,9 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+
+print("Test 1: ", safe_print_integer(42))  # Doit imprimer "42" et retourner True
+print("Test 2: ", safe_print_integer("42"))  # Doit retourner False car "42" est une chaîne, pas un entier
+print("Test 3: ", safe_print_integer("abc"))  # Doit retourner False car "abc" n'est pas un entier
+print("Test 4: ", safe_print_integer([1, 2]))  # Doit retourner False car ce n'est pas un entier
+print("Test 5: ", safe_print_integer(None))
