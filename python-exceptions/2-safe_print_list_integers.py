42a42f31c6b47c0c9effe197010e34392506e9eb
Vitushan <vitushan@outlook.fr>
2025-01-23 11:11:17 +0100

    try / except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    
+    i = 0
+    count = 0
+    try:
+        while i < x:
+            print(my_list[count])
+            count += 1
+            return count
+    except Exception as e:
+        pass
+        print(test(e))
