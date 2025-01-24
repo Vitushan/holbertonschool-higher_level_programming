9aed302c15105fdbf0375318cbb033ae31a4a46b
Vitushan <vitushan@outlook.fr>
2025-01-24 10:24:44 +0100

    try / except

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_[2]
+        except ZeroDivisionError:
+            if result == 0:
+        raise:
+            print("wrong type")    
+        finally:
+            if i < len(my_list_1) or i > len(my_list_2)
+            print("out of range")
+        return new_list
     
