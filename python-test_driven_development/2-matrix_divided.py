654a92c4200a757285b1b8cd1b0a46d66c1c07ff
Vitushan <vitushan@outlook.fr>
2025-01-26 00:53:53 +0100

    try / except

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if not isinstance(matrix(int, float)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    all_element =len(matrix[i])
+    return all_element
