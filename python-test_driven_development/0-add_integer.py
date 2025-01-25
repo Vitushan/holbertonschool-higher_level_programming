126e530970dd47b2b288c7ceeb406ccce7a929b3
Vitushan <vitushan@outlook.fr>
2025-01-26 00:12:12 +0100

    try / except

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -19,6 +19,4 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
-    return a + b
+    return int(a) +int(b)
