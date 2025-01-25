86d6a90b2c713605a2a3252dc9bb282f8edb8261
Vitushan <vitushan@outlook.fr>
2025-01-26 00:26:12 +0100

    try / except

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,20 +3,20 @@
 Module 0-add_integer
 This module provides a function `add_integer`that adds two numbers.
 """
-def add_integer(a, b=98):
-    """
-    Add two integers or floats, converting them to integers if necessary.
-    Args:
+    def add_integer(a, b=98):
+        """
+        Add two integers or floats, converting them to integers if necessary.
+        Args:
         a (int, float): The first number.
         b (int, float): The second number, default is 98.
-    Returns:
+        Returns:
         int: The sum of a and b as integers.
-    Raises:
+        Raises:
         TypeError: If a or b is not an integer or float.
-    """
+        """
     if not isinstance(a, (int, float)):
-        raise TypeError ("must be an integer")
+        raise TypeError("must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    return int(a) +int(b)
+    a = int(a)
+    b = int(b)
