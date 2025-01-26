d80f8c0acf9a0d2aadfc7a7d937307e2bdc43b84
Vitushan <vitushan@outlook.fr>
2025-01-26 01:20:48 +0100

    try / except

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,11 +16,17 @@ def add_integer(a, b=98):
         int: The sum of a and b as integers.
     Raises:
         TypeError: If a or b is not an integer or float.
+        OverflowError: If a or b is infinty.
+        ValError: If a or b is NaN.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+    if a in [float('-inf')] or b in [float('inf'), -float('inf')]:
+        raise OverflowError("cannot convert float infinity to integer")
+    if a != a or b != b:
+        raise ValueError("cannot convert NaN to integer")
     a = int(a)
     b = int(b)
     return a + b
