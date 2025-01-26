4b64116f5c29265b8a12ef6e6ca04f7f67ce7897
Vitushan <vitushan@outlook.fr>
2025-01-26 19:33:06 +0100

    fix

--- python-test_driven_development/tests/6-max_integer.py
+++ python-test_driven_development/tests/6-max_integer.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Module pour trouver l'entier maximal dans une liste"""
-
 def max_integer(list=[]):
-    """Fonction qui trouve et retourne l'entier maximal dans une liste d'entiers
-        Si la liste est vide, la fonction retourne None
+    """
+    Function that finds and returns the maximum integer in a list of integers.
+    If the list is empty, the function returns None.
     """
     if len(list) == 0:
         return None
+
     result = list[0]
     i = 1
+
     while i < len(list):
         if list[i] > result:
             result = list[i]
         i += 1
+
     return result
