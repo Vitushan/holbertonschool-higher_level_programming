d87755efc2854d758b165253028f635a78d1239f
Vitushan <vitushan@outlook.fr>
2025-01-26 19:51:24 +0100

    fix

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -3,7 +3,7 @@
 Unittest for max_integer([..])
 """
 import unittest
-from 6_max_integer import max_integer
+from 6-max_integer import max_integer
 class TestMaxInteger(unittest.TestCase):
     """Test cases for the max_integer function"""
 
