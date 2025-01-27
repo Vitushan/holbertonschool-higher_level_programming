77171e38a955b93bf04a307bf9671bd2574b21d2
Vitushan <vitushan@outlook.fr>
2025-01-27 16:33:41 +0100

    new project

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,9 +6,13 @@ This is a module of square class based on last task.
 
 class Square:
     """
-    Private instance attribute: size.
-    Instantiation with
-    size (no type/value verification)
+    This class defines a square with a private instance attribute: size.
+    Instantiation is done with size, without type or value verification.
     """
+
     def __init__(self, size):
         self.__size = size
+    """
+    Initializes a new Square instance with the given size.
+    The size is stored as a private attribute.
+    """
