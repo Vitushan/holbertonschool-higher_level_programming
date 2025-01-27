be0ca67142c07fc5b16576edd9cbf7ec21f87c42
Vitushan <vitushan@outlook.fr>
2025-01-27 16:29:20 +0100

    new project

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+This is a module of square class based on last task.
+"""
+
+class Square:
+    """
+    Private instance attribute: size.
+    Instantiation with
+    size (no type/value verification)
+    """
+    def __init__(self, size):
+        self.__size = size
