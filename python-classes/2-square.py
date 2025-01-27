a5f334c78f090b4e4fb20c6fa54bd91f0f1e5d82
Vitushan <vitushan@outlook.fr>
2025-01-27 16:47:53 +0100

    fix

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+This is a module of square class based on last task.
+"""
+
+class Square:
+    """
+    This class defines a square with a private instance attribute: size.
+    Instantiation is done with size, without type or value verification.
+    """
+
+    def  __init__(self, size=0):
+        if not isinstance (size, int):
+            raise TypeError:("size must be an integer")
+       
+       
+        if size < 0:
+            raise ValueError:("size must be >= 0")
+
+        self.__size = size
