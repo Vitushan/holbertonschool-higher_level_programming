cb497954482136b96542239d43b991c4058e40b1
Vitushan <vitushan@outlook.fr>
2025-01-27 20:22:05 +0100

    fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,36 +14,36 @@ class Square:
     -method to calculate the area of th square
     """
 
-def __init__(self, size=0):
-    '''
-    Initializes the Square with an optional size (default is 0).
-    Validates that size is an integer and >= 0.
-    '''
-    self.size = size
-@property
-def size(self):
-    """
-    Getter for the size attribute.
-    Returns the private attribute __size.
-    """
-    return self.__size
+    def __init__(self, size=0):
+        '''
+        Initializes the Square with an optional size (default is 0).
+        Validates that size is an integer and >= 0.
+        '''
+        self.size = size
+    @property
+    def size(self):
+        """
+        Getter for the size attribute.
+        Returns the private attribute __size.
+        """
+        return self.__size
 
-@size.setter
-def size(self, value):
-    """
-    Setter for the size attribute.
-    Validates that value is an integer and >= 0.
-    """
+    @size.setter
+    def size(self, value):
+        """
+        Setter for the size attribute.
+        Validates that value is an integer and >= 0.
+        """
 
-    if not isinstance(value, int):
-        raise TypeError("size must be an integer")
-    if value < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-def area(self):
-    """
-    Public method to calculatethe area of the squarz.
-    Returns: the current area of the square (size ** 2).
-    """
-    return self.__size ** 2
+    def area(self):
+        """
+        Public method to calculatethe area of the squarz.
+        Returns: the current area of the square (size ** 2).
+        """
+        return self.__size ** 2
