dc3851499efabe0220e60dfbd693b7f605649640
Vitushan <vitushan@outlook.fr>
2025-01-27 15:37:44 +0100

    new project

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class Square:
-
     """
-    An empty class that defines a square.
+    This is an empty class that defines a square.
+
+    Attributes:
+        None
     """
     pass
