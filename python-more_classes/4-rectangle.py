0703094bcc9eb74ff868558dd6c49fba81077e1e
Vitushan <vitushan@outlook.fr>
2025-01-28 22:42:17 +0100

    fix

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this is a module for defining a rectangle class
+"""
+class Rectangle:
+     """
+    A class to represent rectangle
+    """
+     def __init__(self, width=0, height=0):
+        """
+        Initialize a rectangle with optional width and height.
+        Validations are performed for the parameters
+        """
+        
