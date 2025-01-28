cceb7654ddc571f790ce4c71f80f9da04c63c2f4
Vitushan <vitushan@outlook.fr>
2025-01-28 19:40:13 +0100

    fix

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+"""
+this is a module for defining a rectangle class
+"""
+
+class rectangle:
+
+    """
+    A class to represent a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a rectangle with optional width and height.
+        Validations are performed for the parameters
+        """
+        self.__width = width
+        self.height = height
+        
+
+    @property
+    def width(self):
+        """
+        Getter for width
+        """
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for width with validation:
+        - must be an integer
+        - must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value > 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter for height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for height with validation
+        must be an integer
+        must be >= 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
