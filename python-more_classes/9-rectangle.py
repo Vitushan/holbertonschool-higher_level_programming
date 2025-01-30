867da0d6fe166f320583eb6eb495de7d3d97743f
Vitushan <vitushan@outlook.fr>
2025-01-30 21:15:15 +0100

    fixing pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Thisvis a module for defining a rectangle class
+This is a module for defining a rectangle class
 """
 
 
@@ -8,7 +8,7 @@ class Rectangle:
     """
     A class to represent rectangle
     """
-    number_of_instanceqs = 0
+    number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
@@ -16,4 +16,53 @@ class Rectangle:
         Initialize a rectangle with optional width and height
         validations are performed for the parameters
         """
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+        getter for width"
+        """
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """
+        returns  a rectangle area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns a rectangle perimeter
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+        print the rectangle with the character "#"
+        """
+        if self.__width == 0 or self.__height:
+            return ""
+        return "\n".join([str(self.print_symbol
+                              ) * self.width for _ in range (self.height)])
+
+    def __repr__(self):
+        """
+        return a string representation of the rectangle
+        to be able to recreate a new instance by using eval
+        """
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
         
