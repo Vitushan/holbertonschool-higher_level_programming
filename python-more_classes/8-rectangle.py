9cf5ec3fcc4ab0e5369bd078e954992b360a70e2
Vitushan <vitushan@outlook.fr>
2025-01-30 20:30:12 +0100

    fixing pycodestyle

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -67,8 +67,8 @@ class Rectangle:
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
-        return "\n".join([str(self.print_symbol) * self.width for _
-    in range(self.height)])
+        return "\n".join([str(
+                self.print_symbol) * self.width for _ in range(self.height)])
 
     def __repr__(self):
         """
@@ -80,7 +80,7 @@ class Rectangle:
     def __del__(self):
         """
         print message "Bye rectangle..."
-        when an instance of Rectangle is deleted 
+        when an instance of Rectangle is deleted
         """
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
