e3d26824f2373450027ec1f4dde6b79b648687b4
Vitushan <vitushan@outlook.fr>
2025-01-30 21:48:00 +0100

    fixing pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -85,9 +85,9 @@ class Rectangle:
         return rect_2
 
 
-@classmethod
-def square(cls, size=0):
-    """
-    returns a new Rectangle instance with width == height == size.
-    """
-    return cls(size, size)
+    @classmethod
+    def square(cls, size=0):
+        """
+        returns a new Rectangle instance with width == height == size.
+        """
+        return cls(size, size)
