d78c9c6447853bf84785ea06895b3180eb48d2a9
Vitushan <vitushan@outlook.fr>
2025-01-30 20:38:41 +0100

    fixing pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""
+Thisvis a module for defining a rectangle class
+"""
+
+
+class Rectangle:
+    """
+    A class to represent rectangle
+    """
+    number_of_instanceqs = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a rectangle with optional width and height
+        validations are performed for the parameters
+        """
+        
