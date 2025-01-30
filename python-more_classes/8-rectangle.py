481070616685c53f0c05fcfabbb76f56889c69d9
Vitushan <vitushan@outlook.fr>
2025-01-30 20:20:09 +0100

    fixing betty

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -63,3 +63,38 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if width == 0 or height == 0:
+            return ""
+        return "\n".join([str(self.print_symbol) * self.width for _
+    in range()self.height])
+
+    def __repr__(self):
+        """
+        return a string representation of the rectangle
+        to be able to recreate a new instance by using eval
+        """
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        """
+        print message "Bye rectangle..."
+        when an instance of Rectangle is deleted 
+        """
+        print("Bye rectangle...")
+        Rectangle.number_of_instances =- 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        that returns the biggest rectangle based on the area
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
