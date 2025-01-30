2a1c845d0ed7cbb7287f62fc8fe30f6866993913
Vitushan <vitushan@outlook.fr>
2025-01-30 21:24:38 +0100

    fixing pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -71,3 +71,18 @@ class Rectangle:
         """
         print("Bye rectangle...")
         Rectangle.number_of_instances =- 1
+
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        that returns the biggest rectangle based on the area
+        """
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
+
+def square(cls, size=0):
+    
