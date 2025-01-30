76e983f90799acca82aeb8ccabd7890a533c54dd
Vitushan <vitushan@outlook.fr>
2025-01-30 22:03:45 +0100

    fixing pycodestyle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -33,6 +33,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -46,7 +47,8 @@ class Rectangle:
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("height must be >=0")
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """
@@ -86,6 +88,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """
         that returns the biggest rectangle based on the area
