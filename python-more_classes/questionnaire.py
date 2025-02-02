9b47dae6366a619e0d9f8a7eeab5a83f0d6f275f
Vitushan <vitushan@outlook.fr>
2025-02-02 15:45:34 +0100

    fix

--- python-more_classes/questionnaire.py
+++ python-more_classes/questionnaire.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+score = 0
 def ask_1():
+    global score
     print("Quelle est la capitale de la france ?")
     print("(a) Marseille")
     print("(b) Nice")
@@ -8,12 +10,14 @@ def ask_1():
     print("(d) Bordeaux")
     print()
     answer = input("your response: ")
-    if answer == "c":
+    if answer.lower() == "c":
         print("good answer")
+        score += 1
     else:
         print("Revoie ta geographie")
 
 def ask_2():
+    global score
     print("Quelle est la capitale de la belgique?")
     print("(a) Gand")
     print("(b) Liege")
@@ -21,12 +25,14 @@ def ask_2():
     print("(d) Brussel")
     print()
     answer = input("your response: ")
-    if answer == "d":
+    if answer.lower() == "d":
         print("good answer")
+        score += 1
     else:
         print("Revoie ta geographie")
 
 def ask_3():
+    global score
     print("Quelle est la capitale de la Croatie  ?")
     print("(a) Zagreb")
     print("(b) Dubrovnik")
@@ -34,12 +40,14 @@ def ask_3():
     print("(d) Ogulin")
     print()
     answer = input("your response: ")
-    if answer == "a":
+    if answer.lower == "a":
         print("good answer")
+        score += 1
     else:
         print("Revoie ta geographie")
 
 def ask_4():
+    global score
     print("Quelle est la capitale du Maroc ?")
     print("(a) Agadir")
     print("(b) Rabat")
@@ -47,11 +55,13 @@ def ask_4():
     print("(d) Al hoceima")
     print()
     answer = input("your response: ")
-    if answer == "b":
+    if answer.lower() == "b":
         print("good answer")
+        score += 1
     else:
         print("Revoie ta geographie")
 ask_1()
 ask_2()
 ask_3()
 ask_4()
+print("vous avez obtenue {} de bonne reponse sur 4!".format(score))
