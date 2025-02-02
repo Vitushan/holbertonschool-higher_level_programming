a7af33231c85be787af2fbd1ed14533b0bf8373c
Vitushan <vitushan@outlook.fr>
2025-02-02 15:16:04 +0100

    fix

---
+++ python-more_classes/questionnaire.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+def ask_1():
+    print("Quelle est la capitale de la france ?")
+    print("(a) Marseille")
+    print("(b) Nice")
+    print("(c) Paris")
+    print("(d) Bordeaux")
+    print()
+    answer = input("your response: ")
+    if answer == "c":
+        print("good answer")
+    else:
+        print("Revoie ta geographie")
+
+def ask_2():
+    print("Quelle est la capitale de la belgique?")
+    print("(a) Gand")
+    print("(b) Liege")
+    print("(c) Antwerpen")
+    print("(d) Brussel")
+    print()
+    answer = input("your response: ")
+    if answer == "d":
+        print("good answer")
+    else:
+        print("Revoie ta geographie")
+
+def ask_3():
+    print("Quelle est la capitale de la Croatie  ?")
+    print("(a) Zagreb")
+    print("(b) Dubrovnik")
+    print("(c) Karlovac")
+    print("(d) Ogulin")
+    print()
+    answer = input("your response: ")
+    if answer == "a":
+        print("good answer")
+    else:
+        print("Revoie ta geographie")
+
+def ask_4():
+    print("Quelle est la capitale du Maroc ?")
+    print("(a) Agadir")
+    print("(b) Rabat")
+    print("(c) Casablanca")
+    print("(d) Al hoceima")
+    print()
+    answer = input("your response: ")
+    if answer == "b":
+        print("good answer")
+    else:
+        print("Revoie ta geographie")
+ask_1()
+ask_2()
+ask_3()
+ask_4()
