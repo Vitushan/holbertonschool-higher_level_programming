e1ba0e5350ae92c143629116114d650269ed158c
Vitushan <vitushan@outlook.fr>
2025-02-03 15:20:01 +0100

    sorted self

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,3 +4,12 @@ This is a module a square class
 """
 
 
+class MyList(list):
+    """
+    this is a class Mylist
+    """
+    def print_sorted(self):
+        """
+        prints the list, but sorted (ascending sort)
+        """
+        print(sorted(self))
