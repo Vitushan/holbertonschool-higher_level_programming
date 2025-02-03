4a74d2aa7ed02c03821017b22b32aca1f297e103
Vitushan <vitushan@outlook.fr>
2025-02-03 19:57:58 +0100

    type training

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,5 +5,5 @@ def is_same_class(obj, a_class):
     Return: True is the object is exactly an instance of the specified class
     Return: False if otherwise.
     """
-
+    
     return type(obj) is a_class
