cf71a0b6034d742ed79f033fc7aa8348d2e47da9
Vitushan <vitushan@outlook.fr>
2025-02-03 19:40:52 +0100

    sorted self

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 """
-This is a module a Mylist class
+This is a module with the  Mylist class
 """
 
 
 class MyList(list):
-    """
-    this is a class Mylist
-    """
     def print_sorted(self):
         """
-        prints the list, but sorted (ascending sort)
+        prints the list in ascending sorted order.
         """
         print(sorted(self))
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-"""
-This is a module a square class
-"""
 
+def is_same_class(obj, a_class):
+    """
+    Return: True is the object is exactly an instance of the specified class
+    Return: False if otherwise.
+    """
+
+
+if is_same_class <= 0:
+    return True
+return False
