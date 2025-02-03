eb40dfedda7baf891a6d16e065967b3945a39a08
Vitushan <vitushan@outlook.fr>
2025-02-03 14:02:33 +0100

    preparing task

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a module a square class
+"""
+
+
+def lookup(obj):
+    """
+    function that returns the list of
+    available attributes and methods of an object
+    """
+
+    return list
