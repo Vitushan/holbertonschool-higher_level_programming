8f855b8dd3a1787ad986bf48c6643eb1de1790e3
Vitushan <vitushan@outlook.fr>
2025-02-03 20:35:37 +0100

    type training

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -5,5 +5,7 @@ Module for checking object inheritance.
 
 
 def is_kind_of_class(obj, a_class):
-
+    """
+    Check if obj is an instance of a_class or its subclass.
+    """
     return isinstance(obj, a_class)
