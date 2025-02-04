c043ea08892d107fb45caf5887a8b88345888a0e
Vitushan <vitushan@outlook.fr>
2025-02-04 09:32:29 +0100

    type training

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@ This is a module a square class
 
 def inherits_from(obj, a_class):
     """
-    Check if obj is an  instance inherits of a_class (directly or indirectly)
+    Check if obj is an  instance of a class that inherited from a_class
     """
 
     if isinstance(obj, a_class) or type(obj) is a_class:
