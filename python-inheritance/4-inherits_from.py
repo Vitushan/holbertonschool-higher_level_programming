19bd326a8ddba5d83b850c2d045ac68c7fa6aead
Vitushan <vitushan@outlook.fr>
2025-02-04 09:35:45 +0100

    type training

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -9,5 +9,5 @@ def inherits_from(obj, a_class):
     Check if obj is an  instance of a class that inherited from a_class
     """
 
-    if isinstance(obj, a_class) or type(obj) is a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
