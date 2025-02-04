5b331577ab373d2ba8491285ed7eeaf69743e3b7
Vitushan <vitushan@outlook.fr>
2025-02-04 17:09:19 +0100

    type training

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -25,15 +25,15 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
 
-        def area(self):
-            """
-            Returns width x height
-            """
-            return self.__width * self.__height
+    def area(self):
+        """
+        Returns width x height
+        """
+        return self.__width * self.__height
 
-        def __str__(self):
-            """
-            Returns
-            the string representation of the rectangle
-            """
-            return f"[Rectangle] {self.__width}/{self.__height}"
+    def __str__(self):
+        """
+        Returns
+        the string representation of the rectangle
+        """
+        return f"[Rectangle] {self.__width}/{self.__height}"
