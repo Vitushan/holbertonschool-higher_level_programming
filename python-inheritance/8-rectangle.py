40fdcebc462e7c6616f9255234d1267226015e27
Vitushan <vitushan@outlook.fr>
2025-02-04 13:44:14 +0100

    type training

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,3 +4,15 @@ This is a module a square class
 """
 
 
+class Rectangle:
+    """
+    This is a Rectangle class
+    """
+    def __init__(self, width, height):
+        """
+        constructor for width and height
+        """
+        if self.__width < 0 and self.__height < 0:
+
+            self.__width = width
+            self.__height = height
