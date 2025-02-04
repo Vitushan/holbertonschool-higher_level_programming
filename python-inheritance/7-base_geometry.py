4841d3a3eab15cb1e36197ca3f1c6417428ab88a
Vitushan <vitushan@outlook.fr>
2025-02-04 10:02:48 +0100

    type training

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,3 +4,22 @@ This is a module a square class
 """
 
 
+class BaseGeometry:
+    """
+    this a class based in previous task
+    """
+    def area(self):
+        """
+        Return a Geometry area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        check if the args is valided integer
+        """
+        if value is not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
+        
