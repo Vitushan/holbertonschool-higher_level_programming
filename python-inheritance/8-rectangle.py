7a0cd9f74ab55369e359c2bd41a6a9b6c32cadea
Vitushan <vitushan@outlook.fr>
2025-02-04 13:54:19 +0100

    type training

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,16 +4,21 @@ This is a module a square class
 """
 
 
+from 7-base_geometry import BaseGeometry
 class Rectangle:
     """
     This is a Rectangle class
+    that inherits from BaseGeometry
     """
 
     def __init__(self, width, height):
         """
-        constructor for width and height
+        constructor for width
+        and height of the rectangle.
+        The width and height must be positive integers.
         """
-        if self.__width < 0 and self.__height < 0:
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
-            self.__width = width
-            self.__height = height
+        self.__width = width
+        self.__height = height
