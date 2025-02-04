4993e66dfdf1ef89499240aa719bc2929932ea08
Vitushan <vitushan@outlook.fr>
2025-02-04 20:22:32 +0100

    type training

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -4,3 +4,26 @@ This is a module a square class
 """
 
 
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """
+    This is a square class
+    that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        constructor for size
+        of the square.
+        The size must be positive integers.
+        """
+
+        self.integer_validator("size", size)
+
+    def area(self):
+        """
+         return square area
+        """
+        return self.__size * self.__size
+
