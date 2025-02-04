a5b4079e86bb0a6db5372a8f92683162cb829819
Vitushan <vitushan@outlook.fr>
2025-02-04 20:28:43 +0100

    type training

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -28,3 +28,11 @@ class Square(Rectangle):
         return square area
         """
         return self.__size * self.__size
+    
+    def __str__(self):
+        """
+        Returns
+        the string representation of the square
+        """
+        return f"[Square] {self.__size}/{self.__size}"
+    
