b80549ced64bdec71cd0cb9e7f5205324895130c
Vitushan <vitushan@outlook.fr>
2025-02-04 17:00:18 +0100

    type training

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -35,4 +35,4 @@ class Rectangle(BaseGeometry):
             Returns
             the string representation of the rectangle
             """
-            return f"[Rectangle] {self.__width} / {self.__height}"
+            return f"[Rectangle] {self.__width}/{self.__height}"
