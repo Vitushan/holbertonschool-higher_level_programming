542367cc4a5b78ca0919f88cbb4ff576bdc2caa2
Vitushan <vitushan@outlook.fr>
2025-02-04 09:49:55 +0100

    type training

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This is a module a square class
+This is a module a  obj class
 """
 
 
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 """
-This is a module a square class
+This is a module a obj class
 """
 
 
+class BaseGeometry:
+    """
+    this a class based in previous task
+    """
+
+    def area(self):
+        """
+        Return a Geometry area
+        """
+    raise TypeError("area() is not implemented")
