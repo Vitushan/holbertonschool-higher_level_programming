2839d85d44986fb565f8da30fc767dff43087965
Vitushan <vitushan@outlook.fr>
2025-02-05 23:10:51 +0100

    preparing all task and test

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This is a module defining an "abc method" class
+with sublasses Dog and Cat
+"""
+
+from abc import ABC, abstractmethod
+import math
--- python-abc/test.py
+++ python-abc/test.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 
 class Animal(ABC):
+
     
     @abstractmethod
     def sound(self):
