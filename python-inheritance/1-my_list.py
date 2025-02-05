e9d3e1dd9d1fb8e30b4f21cb60241b09010faca3
Vitushan <vitushan@outlook.fr>
2025-02-05 10:15:02 +0100

    fix txt file

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-This is a module with the  Mylist class
+This is a module with the  "Mylist" class
 """
 
 
 class MyList(list):
+    """
+    this is a class "MyList" in inheritage of "list"
+    """
     def print_sorted(self):
         """
         prints the list in ascending sorted order.
