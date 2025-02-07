1c058753f94b0645e2ccefa172ff8ef31dd86b09
Vitushan <vitushan@outlook.fr>
2025-02-07 13:52:38 +0100

    preparing all task and test

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+...
+"""
+
+
+class CountedIterator:
+    """
+    count iterator
+    """
+
+    def __init__(self, iterator):
+        self.iterator = iter(iterator)
+        self.count = 0
+    
+    def __next__(self):
+        """
+        ...
+        """
+        obj = next(self.iterator)
+        self.count += 1
+        return obj
+        if obj > len(obj):
+            raise StopIteration
+
+    def get_count(self):
+        """
+        ...
+        """
+        return self.count
