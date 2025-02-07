84f507de1fc535b51d83938655e0370760d58346
Vitushan <vitushan@outlook.fr>
2025-02-07 08:03:35 +0100

    preparing all task and test

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 """
-add functionality to classes in a modular fashion
+Design two mixin classes, SwimMixin and FlyMixin
 """
 
 
+class SwimMixin:
+    """
+    Mixin add the capacity for swim
+    """
+    def swim(self):
+        print("The creature swims!")
 
+
+class FlyMixin:
+    """
+    add the capacity for fly
+    """
+    def fly(self):
+        print("The creature flies!")
+
+class Dragon(SwimMixin, FlyMixin):
+    """
+    Class dragon that can swim and fly
+    """
+    def roar(self):
+        print("roar")
