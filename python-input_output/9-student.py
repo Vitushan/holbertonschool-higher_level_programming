5f75e01ed288786f29d9370665c45af85ca86f21
Vitushan <vitushan@outlook.fr>
2025-02-10 21:13:15 +0100

    return dict

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+Class student defining a Student
+"""
+
+
+class Student:
+    """
+    Class student defining a Student
+    """
+
+
+    def __init__(self, first_name, last_name, age):
+        """
+        ...
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        return a dictionary representing a student class
+        """
+        return Student.__dict__
