af59ae0a2ae0ce8ed37e57ab86c625b3ca08232b
Vitushan <vitushan@outlook.fr>
2025-02-10 13:52:07 +0100

    fixxing final

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+...
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    append a string end returns the number of characters
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)
