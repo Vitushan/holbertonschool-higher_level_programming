7ac78d24ef4b08105b1b24dcebb87685be2f3333
Vitushan <vitushan@outlook.fr>
2025-02-10 21:27:10 +0100

    return dict

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""
+...
+"""
+
+
+class Student:
+    """
+    ...
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        initializing name, last name and student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        ...
+        """
+        attrs.__dict__
+        if attrs is str:
+            return self.attrs
