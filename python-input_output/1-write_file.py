3db13d32e9ac609651c4f56c3e59306d9acd1456
Vitushan <vitushan@outlook.fr>
2025-02-10 10:43:14 +0100

    read utf-8 file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+this is a module for write in the file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    for write ont the file in utf-8 encoding
+    """
+
+    with open(filename, "w", encoding="8-utf") as f:
+        print(f.write(), end="")
