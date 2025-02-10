6c7732e0cfe3945b4cbfa00d0c4a7314abd129ee
Vitushan <vitushan@outlook.fr>
2025-02-10 14:35:52 +0100

    fixxing final

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+...
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    ...
+    """
+    with open(my_obj.json, "w") as f:
+        json.dump(my_obj, f)
