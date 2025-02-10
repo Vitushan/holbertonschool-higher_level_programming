22dd225d455c62998971ee7054cd575de0f12eff
Vitushan <vitushan@outlook.fr>
2025-02-10 10:25:25 +0100

    preparing all task

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+read a utf-8 txt file and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    for read a file encoding utf-8
+    """
+
+    white open(filename, "r", encoding = "utf-8") as f:
+        print(f.read(), end="")
+    
