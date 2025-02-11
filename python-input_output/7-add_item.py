b1621bc3335387f51e7dd0e80badbed34249b940
Vitushan <vitushan@outlook.fr>
2025-02-11 10:18:41 +0100

    convert json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 """
-...
+Script add all arg on the list
 """
 import sys
 import json
+import os
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+filename = "add_item.json"
+
+if os.path.exists(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        item = json.load(f)
+else:
+    item = []
+
+item.extend(sys.argv[1:])
+
+with open(filename, "w") as file:
+    json.dump(item, file)
