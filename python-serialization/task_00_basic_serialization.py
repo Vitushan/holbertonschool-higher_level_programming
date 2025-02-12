be5bd7dabdb886906bab4fa71b6c041c1efe8c49
Vitushan <vitushan@outlook.fr>
2025-02-12 14:21:11 +0100

    fixxing final

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,3 +4,19 @@
 """
 
 
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    ...
+    """
+    with open(filename, 'w', encoding="utf-8"):
+        json.dumps(filename)
+
+
+def load_and_deserialize(filename):
+    """
+    ...
+    """
+    json.loads(filename)
