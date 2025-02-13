ec8ae31f46de74290bfaff704b90876fcb55c0ac
Vitushan <vitushan@outlook.fr>
2025-02-13 08:26:30 +0100

    dur

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -15,7 +15,7 @@ def convert_csv_to_json(csv_filename):
     try:
         with open(csv_filename, mode="r", encoding="utf-8") as csv_file:
             csv.DictReader(csv_file)
-            data = [row for row in reader]
+            data = [row for row in csv.DictReader(csv_file)]
 
         with open("data.json", mode="w", encoding="utf-8") as json_file:
             json.dump(data, json_file, indent=4)
