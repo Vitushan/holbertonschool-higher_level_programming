cdb70a58e639deac2224ce5587f685c2431befcd
Vitushan <vitushan@outlook.fr>
2025-02-13 21:31:54 +0100

    dur

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """
-...
+pascal triangle
 """
 
 def pascal_triangle(n):
     """
-    ...
+    every line start by 1 and every elements inside 1 their sum is greater
     """
-
-    
+    def pascal_triangle(n):
+        """
+        return a list of the list representing pascal triangle
+        """
+        if n <= 0:
+            return []
