66da6c0891209eb069b7aed3a2cd7f5535fe5811
Vitushan <vitushan@outlook.fr>
2025-02-18 16:53:41 +0100

    preparing new directory for learn API restful

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -3,9 +3,9 @@
 ...
 """
 
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import json
+from http.server import BaseHTTPRequestHandler
 import socketserver
+import json
 
 
 class SimpleServer(BaseHTTPRequestHandler):
@@ -27,7 +27,7 @@ class SimpleServer(BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode("utf-8"))
 
         elif self.path == "/status":
             self.send_response(200)
@@ -39,15 +39,15 @@ class SimpleServer(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
-            info = {"version": "1.0", "description": "A simple API built with http.server"}
-            self.wfile.write(json.dumps(info).encode())
+            info = {"version": "1.0", "description": 
+                    "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode("utf-8"))
 
         else:
             self.send_response(404)
-            self.send_header("Content-type", "application/json")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            error_message = {"error": "Endpoint not found"}
-            self.wfile.write(json.dumps(error_message).encode())
+            self.wfile.write(b"Endpoint not found")
 
 PORT = 8000
 with socketserver.TCPserver(("", PORT), SimpleServer) as httpd:
