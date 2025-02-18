a09457ccd3825dee221889b655beb408f97b76d0
Vitushan <vitushan@outlook.fr>
2025-02-18 16:12:47 +0100

    preparing new directory for learn API restful

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,18 +4,51 @@
 """
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+
 
 class SimpleServer(BaseHTTPRequestHandler):
     """
     create a simple  HTTP server
     """
     def do_GET(self):
-        self.send_response(404)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello, this is a simple API!")
+        """
+        Handle Get and serves differen respinses based on the endpoint.
+        """
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
 
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            info = {"Version": "1.0", "description:" "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            error_message = {"error": "Endpoint not found"}
+            self.wfile.write(json.dumps(error_message).encode())
 server_address = ("", 8000)
 httpd = HTTPServer(server_address, SimpleServer)
 
+print("Server started on port 8000")
 httpd.serve_forever()
