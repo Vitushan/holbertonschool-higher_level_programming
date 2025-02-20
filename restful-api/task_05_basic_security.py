9ce7745e596ebe24badd404aaf651abf43492836
Vitushan <vitushan@outlook.fr>
2025-02-20 22:07:21 +0100

    re

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,1 +1,65 @@
 #!/usr/bin/python3
+"""
+security API with Flask
+"""
+from flask import Flask, jsonify, request
+from flask_httpauth  import HTTPBasicAuth
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import timedelta
+
+app = Flask(__name__)
+
+app.config["JWT_SECRET_KEY"] = "supersecretkey"
+app.config["JWT ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+
+
+jwt = JWTManager(app)
+auth = HTTPBasicAuth()
+
+users = {
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("adminpass"), "role": "admin"}
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username is users and check_password_hash(users[username]["password"], password):
+        return username
+    return None
+
+@app.route("/basic-protected", methods=["GET"])
+@auth.login_required
+def basic_protected():
+    return jsonify({"message": "Basic auth: Access Granted"}), 200
+    
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"error": "Username and password required"}), 400
+
+    user = users.get(username)
+    if not user or not check_password_hash(user["password"], password):
+        return jsonify({"error": "Invalid credentials"}), 401
+
+    access_token = create_access_token(identify={"username": username, "role": user["role"]})
+    return jsonify({"access_token": access_token}), 200
+
+@app.route("/jwt-protected", methods["GET"])
+@jwt_required()
+def jwt_protected():
+    return jsonify({"message": "JWT Auth: Access Granted"}), 200
+
+@app.route("/admin-only", methods=["GET"])
+@jwt_required()
+def admin_only():
+    current_user = get_jwt_identity()
+
+    if current_user["role"] != "admin":
+        return jsonify({"error": "Admin access required"}), 403
+    return jsonify({"message": "Admin Access: Granted"}), 200
+
