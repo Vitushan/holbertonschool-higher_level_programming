fd704554e7fbb5f1d8d0f7226444547517cb26e0
Vitushan <vitushan@outlook.fr>
2025-03-03 23:24:46 +0100

    reparing

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
---lists all record of the second_table
---display the score and the name  by descending order
+-- lists all record of the second_table
+-- display the score and the name  by descending order
 SELECT score, name
 FROM second_table
 WHERE  name != '' AND name IS NOT NULL
