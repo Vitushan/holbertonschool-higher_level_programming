2cea47c239be2ecb32282a285e5c4f847a053510
Vitushan <vitushan@outlook.fr>
2025-03-03 14:51:48 +0100

    reparing

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Called first_table in the current database
 -- first_table description : id: INT; name VARCHAR (256)
-IF CREATE TABLE NOT EXISTS first_table(id INT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
