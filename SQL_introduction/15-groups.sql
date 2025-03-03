c40a4dd857c3629fd36f9a76ae86a192c2264c48
Vitushan <vitushan@outlook.fr>
2025-03-03 22:44:42 +0100

    reparing

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- script tha lists the number of record with the same score
 -- in the second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS NUMBER
+SELECT score, COUNT(*) AS number
 FROM second_table GROUP BY score
 ORDER BY count DESC;
