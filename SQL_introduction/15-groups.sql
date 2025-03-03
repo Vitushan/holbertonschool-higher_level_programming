8df8f2e15a411a00bafab5f31a3ea9f66bff31ab
Vitushan <vitushan@outlook.fr>
2025-03-03 22:52:42 +0100

    reparing

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- script tha lists the number of record with the same score
 -- in the second_table of the database
+UPDATE score SET name = 'number' WHERE second_table 
 SELECT score COUNT(*) AS count
 FROM second_table
 GROUP BY score
