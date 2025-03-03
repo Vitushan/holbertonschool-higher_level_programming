601827160f98a094fb40a8d5c41887111cd057d0
Vitushan <vitushan@outlook.fr>
2025-03-03 17:32:38 +0100

    reparing

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- that computes the score average of all record in the second_table
 --the result column name should be average
-INSERT INTO (name) VALUES('average')
+INSERT AVG(score) AS average
 FROM second_table;
