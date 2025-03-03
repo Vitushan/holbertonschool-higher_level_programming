bb0547793bf171d5d9d9d95412c68ad5932f690e
Vitushan <vitushan@outlook.fr>
2025-03-03 16:08:08 +0100

    reparing

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
 -- create a table second_table in the database
 -- in MySQL server and add multiples rows
-CREATE TABLES IF NOT EXISTS second_table (INT id, name VARCHAR(256), INT score);
+CREATE TABLE IF NOT EXISTS second_table(id INT PRIMARY KEY, name VARCHAR(256), score INT);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, Bob, 14), (4, 'George', 8);
