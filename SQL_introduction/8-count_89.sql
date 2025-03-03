3aa97a5c01a197f7ff55cfbcb801adddad048de2
Vitushan <vitushan@outlook.fr>
2025-03-03 15:52:24 +0100

    reparing

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- script that display the number of records
 -- id = 89 in the table first_table of the database hbtn_0c_0
-SELECT COUNT(id=89) FROM first_table;
+SELECT COUNT(*) FROM first_table WHERE id = 89;
