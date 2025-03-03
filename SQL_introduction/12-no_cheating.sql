efa95277b7bac7ca724a2898255d4b734b1e39af
Vitushan <vitushan@outlook.fr>
2025-03-03 17:12:59 +0100

    reparing

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,3 @@
 -- Updates the score of bob to 10 in the second table
 -- with UPDATE command
-UPDATE second_table set name = 'bob' WHERE score=10
+UPDATE second_table set name = 'bob' WHERE score=10;
