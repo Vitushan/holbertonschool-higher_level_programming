6a33c33fee3f27b5f2860ea1508986cfbcc16f47
Vitushan <vitushan@outlook.fr>
2025-03-04 10:44:18 +0100

    reparing

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,7 @@
 -- Creates the MySQL server user user_0d_1
 -- if the user already exists, this script should not fail
 CREATE USER IF NOT EXISTS
-'user_0d_1'@'localhost' IDENTIFIED BY
-'user_0d_1_pwd';
+'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO
 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES
