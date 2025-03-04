c57e0e82f823ece32cb91e8017123af8a272b8e1
Vitushan <vitushan@outlook.fr>
2025-03-04 14:27:06 +0100

    reparing

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,4 @@
+-- Create table: id_not_null
+-- if the table already exists, this script should not fail
+CREATE TABLE id_not_null EXISTS
+CREATE TABLE IF NOT EXISTS id_not_null (id INT, name VARCHAR256);
