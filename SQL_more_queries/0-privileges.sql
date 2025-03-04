aa8df639ff0228030ce1c20c38b01b3320fb021d
Vitushan <vitushan@outlook.fr>
2025-03-04 09:49:51 +0100

    preparing project 1

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of the MySQL
 -- with command GRANT for user 1 and 2.
-SHOW ALL GRANT user_0d_1 AND user_0d_2;
+SHOW ALL GRANT 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
