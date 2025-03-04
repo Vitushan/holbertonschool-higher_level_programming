87168b7085ae1d37994340d74adc8a87926191bd
Vitushan <vitushan@outlook.fr>
2025-03-04 13:43:00 +0100

    reparing

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- print privileges for user_0d_1
--- print privileges for user_0d_2
-IF user_0d_2 EXISTS
-IF hbtn_0d_2 EXISTS
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS INSERT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
+-- CREATE for user_0d_2 with password
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES
