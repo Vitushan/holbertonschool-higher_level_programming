1d054e2114a53d8d92e98e4467b8cc0d1d851325
Vitushan <vitushan@outlook.fr>
2025-03-04 13:57:12 +0100

    reparing

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
 -- Create a new table:
 --@:force_name"
 IF force_name EXISTS
-CREATE TABLE (id INT, NAME VARCHAR(256)) VALUES(1, 'force_name');
+CREATE TABLE (id INT, NAME VARCHAR(256)) VALUES(1, 'force_name') ON hbtn_0d_2;
