528566cf881248007fb55ffbbf5e9dc5a496cc26
Vitushan <vitushan@outlook.fr>
2025-03-04 13:15:47 +0100

    reparing

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- print privileges for user_0d_1
 -- print privileges for user_0d_2
 IF user_0d_2 EXISTS
-FROM hbtn_0d_2;
+IF hbtn_0d_2 EXISTS
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS SELECT FOR 'user_0d_2'@'localhost';
