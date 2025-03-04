626fab1bc91a3f0760f8e975c48bd79f767e6d62
Vitushan <vitushan@outlook.fr>
2025-03-04 09:51:32 +0100

    reparing

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privileges of the MySQL
 -- with command GRANT for user 1 and 2.
-SHOW ALL GRANT FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
+SHOW GRANT FOR 'user_0d_1'@'localhost' AND 'user_0d_2'@'localhost';
