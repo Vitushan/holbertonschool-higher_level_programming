e69a212f7eba9a277edf0e3b165414c1eb07e38a
Vitushan <vitushan@outlook.fr>
2025-03-04 13:20:21 +0100

    reparing

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,4 @@
 IF user_0d_2 EXISTS
 IF hbtn_0d_2 EXISTS
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS PRIVILEGES SELECT ON 'user_0d_2'@'localhost';
+SHOW GRANTS INSERT ON hbtn_0d_2 TO 'user_0d_2'@'localhost';
