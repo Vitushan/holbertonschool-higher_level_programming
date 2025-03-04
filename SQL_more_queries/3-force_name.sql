8c0d99e32607ec4a20e9cc8e2d467703ca68e09c
Vitushan <vitushan@outlook.fr>
2025-03-04 14:11:37 +0100

    reparing

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- Create a new table force_name
--- create a table if not exist
+-- Create a table if not exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR((256)) NOT NULL
