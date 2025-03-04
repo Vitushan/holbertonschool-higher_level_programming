b901cacc057e5bb8efe51dda66e5ca3212faa19e
Vitushan <vitushan@outlook.fr>
2025-03-04 15:00:54 +0100

    reparing

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,6 @@
 -- create the unique_id table
 -- description: id INT with default value 1 and unique
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT,
-    DEFAULT VALUE (1),
+    id INT DEFAULT VALUE (1),
     name VARCHAR(256)
 );
