d0b72aebe9980927773bff79df90effec691b132
Vitushan <vitushan@outlook.fr>
2025-03-04 11:22:19 +0100

    reparing

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,4 @@
--- Creates the databases
--- create user_0d_2
-CREATE DATABASES IF NOT EXISTS
-'hbtn_0d_2';
-CREATE USER IF NOT EXISTS
-'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO
-'user_0d_2'@'localhost';
+-- print privileges for user_0d_1
+-- print privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
