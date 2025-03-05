fcbff43d0bd1b280893e2ac94af0beafa26d2014
Vitushan <vitushan@outlook.fr>
2025-03-05 10:47:48 +0100

    reparing

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists all cities contained in the hbtn_0d_usa database
+-- results must be sorted in ascending order by cities.id
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = state_id
+ORDER BY cities.id;
