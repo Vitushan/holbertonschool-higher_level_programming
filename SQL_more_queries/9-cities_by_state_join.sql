b2aeffca90efad6c4f327b7869e31dd58dae1aff
Vitushan <vitushan@outlook.fr>
2025-03-05 10:51:57 +0100

    reparing

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -2,5 +2,5 @@
 -- results must be sorted in ascending order by cities.id
 SELECT cities.id, cities.name, states.name
 FROM cities, states
-WHERE cities.state_id = state_id
+WHERE cities.state_id = states.id
 ORDER BY cities.id;
