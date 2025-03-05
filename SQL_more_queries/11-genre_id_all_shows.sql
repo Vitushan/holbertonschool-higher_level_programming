b38660df1f90ad1a8e379b3351b4974b2405b06f
Vitushan <vitushan@outlook.fr>
2025-03-05 18:28:29 +0100

    tsk11

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- import the database dump of hbtn_0d_tvshows
+-- lists all show contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+ORDER BY tv_shows.title AND tv_show_genres.genre_id;
