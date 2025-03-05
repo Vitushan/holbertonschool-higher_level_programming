9eae37edd8a2d4d252440eaa006c2d3dce4a5f73
Vitushan <vitushan@outlook.fr>
2025-03-05 11:48:45 +0100

    reparing

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
 -- import the database dump from hbtn_0d_tvshows
 -- listes all shows contained in hbtn_0d_tvshows
 CREATE DATABASE IF NOT EXISTS hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.tvgenre_id
 FROM hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
-JOIN tv_shows.title = tv_show_genres.genre_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
