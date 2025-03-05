05b3d41cfedc0785f8e6cb23c02fc8013946cb65
Vitushan <vitushan@outlook.fr>
2025-03-05 19:10:32 +0100

    tsk11

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -2,5 +2,5 @@
 -- lists all show contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_shows.title ON tv_show_genres = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.shows_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
