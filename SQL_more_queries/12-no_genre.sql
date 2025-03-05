d42b867263a77d0e539639704aab389a6ef87c70
Vitushan <vitushan@outlook.fr>
2025-03-05 19:53:29 +0100

    tsk11

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,6 @@
 -- the database name will be passed as an argument of the mysql command
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN INNER tv_show_genres = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
