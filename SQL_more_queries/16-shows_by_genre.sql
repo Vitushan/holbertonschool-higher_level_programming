9de8661a06ea5201c1f07d78993b240a14e83805
Vitushan <vitushan@outlook.fr>
2025-03-06 11:08:31 +0100

    tsk11

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- lists all show
 -- and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_show.title, tv_genre.name
+SELECT tv_shows.title, tv_genre.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+ORDER BY tv_shows.title ASC,
+tv_genres.name ASC;
