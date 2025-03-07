198b847c895b99883fcde23a3f5dc3332d8c563a
Vitushan <vitushan@outlook.fr>
2025-03-07 14:11:24 +0100

    re

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+script lists all states with a name
+starting with N from rhe database
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db=MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3]
+)
+
+cur = db.cursor()
+cur.execute("SELECT * FROM STATES ORDER BY 'N'")
+
+for rows in cur.fetchall():
+    print(rows)
+
+cur.close()
+db.close()
