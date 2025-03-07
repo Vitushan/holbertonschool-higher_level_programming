b34ec271a7cff46c39e774cdf8e2766d395cc3e1
Vitushan <vitushan@outlook.fr>
2025-03-07 14:18:07 +0100

    re

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
-)
+    )
 
 cur = db.cursor()
+
 cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
 for row in cur.fetchall():
