042e6456604693c025e532707a5c1e52e117cd77
Vitushan <vitushan@outlook.fr>
2025-03-07 14:58:14 +0100

    re

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
