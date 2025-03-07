d643d2fd148c678a8374763be092f75ada4c358d
Vitushan <vitushan@outlook.fr>
2025-03-07 23:26:01 +0100

    re

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
-...
-"""
-
-
-#!/usr/bin/python3
-"""
-Liste tous les objets State de la base de données hbtn_0e_6_usa.
+Lists all objects state of the database.
 """
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State  # Importation des classes nécessaires
+from model_state import Base, State
+
 
 if __name__ == "__main__":
-    # 1️⃣ Connexion à la base de données MySQL avec SQLAlchemy
+
     engine = create_engine(f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}", pool_pre_ping=True)
 
-    # 2️⃣ Création d'une session pour interagir avec la base de données
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # 3️⃣ Récupération et affichage des objets State triés par id
     for state in session.query(State).order_by(State.id):
         print(f"{state.id}: {state.name}")
 
-    # 4️⃣ Fermeture de la session
     session.close()
