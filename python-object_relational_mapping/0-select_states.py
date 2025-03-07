754f69b5357b9a4ab5869d823404e86ab93c1ca6
Vitushan <vitushan@outlook.fr>
2025-03-07 11:52:52 +0100

    preparing last project

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,8 @@ if __name__ == "__main__":
                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    [print(state) for state in cur.fetchall()]
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
