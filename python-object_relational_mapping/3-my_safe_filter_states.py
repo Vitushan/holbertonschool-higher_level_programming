e5be5338b742f856ca35573bc3fd8e5065f2a909
Vitushan <vitushan@outlook.fr>
2025-03-07 15:24:44 +0100

    re

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     query = "SELECT * FROM states WHERE name = %s\
-        ORDER BY id ASC".format(state_name)
+        ORDER BY id ASC"
+
     cur.execute(query, (state_name,))
 
     for row in cur.fetchall():
