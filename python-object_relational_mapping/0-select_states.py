5bce5940cdf8d60a294152741f87e47b9fbccbe9
Vitushan <vitushan@outlook.fr>
2025-03-07 12:17:34 +0100

    re

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Script qui liste tous les États de la base de données hbtn_0e_0_usa.
+"""
+
 import MySQLdb
 import sys
 
@@ -11,13 +15,15 @@ if __name__ == "__main__":
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
+
     cur = db.cursor()
 
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    rows = cur.fetchall()
-    for row in rows:
+
+    for row in cur.fetchall():
         print(row)
+
 
     cur.close()
     db.close()
