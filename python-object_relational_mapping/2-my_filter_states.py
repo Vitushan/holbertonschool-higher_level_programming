a950e8413b7ed7084fb715281e427a41bdfd228b
Vitushan <vitushan@outlook.fr>
2025-03-07 15:11:44 +0100

    re

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     query = "SELECT * FROM states WHERE name = '{}'\
         ORDER BY id ASC".format(state_name)
-    cur.execute(query)
+    cur.execute(query, (state_name,))
 
     for row in cur.fetchall():
         print(row)
