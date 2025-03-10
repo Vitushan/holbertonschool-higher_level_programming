a25d35f40310ccb8ad3017ea0e2ed60ced20b0cd
Vitushan <vitushan@outlook.fr>
2025-03-10 07:35:17 +0100

    pycodestyle

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Class definition of a State and an instance."""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+"""
+model_city fetch by states
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+from model_city import City
 
-Base = declarative_base()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
+    cities = session.query(City, State).join(State).order_by(City.id).all()
+    for city, state in cities:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
-class State(Base):
-    """Class that displays state in the database."""
-
-    __tablename__ = "states"
-
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
+    session.close()
