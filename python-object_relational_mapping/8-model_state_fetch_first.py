73faae695f646907436ca88f2d0832093312ac67
Vitushan <vitushan@outlook.fr>
2025-03-10 07:08:12 +0100

    pycodestyle

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,7 +16,6 @@ if __name__ == "__main__":
 
     first_state = session.query(State).order_by(State.id).first()
 
-
     if first_state:
         print("{}: {}".format(first_state.id, first_state.name))
     else:
