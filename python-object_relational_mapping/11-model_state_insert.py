39c8773aeeddf024d1d9fa76e40e5780e76d4436
Vitushan <vitushan@outlook.fr>
2025-03-10 07:18:47 +0100

    pycodestyle

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -14,12 +14,11 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-
     session.add(new_state)
+
     session.commit()
 
     print(new_state.id)
