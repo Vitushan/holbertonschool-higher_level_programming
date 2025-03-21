fa4835b11d34b891ccc1a2bcece3abb8d6546af9
Vitushan <vitushan@outlook.fr>
2025-03-21 15:38:03 +0100

    invitation generate

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,3 +5,10 @@ def generate_invitations(template, attendees):
     """
     generate invitation files from template and attendees
     """
+
+    if not isinstance(template, str):
+        print("Error: Template must be a string")
+        return
+    if not isinstance(attendees, list):
+        print("Error: attendees must be a list of dictionaries")
+        return
