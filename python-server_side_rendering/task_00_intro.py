01c09dbe76c42d31467ad701f518f7108235e543
Vitushan <vitushan@outlook.fr>
2025-03-21 15:51:02 +0100

    invitation generate

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -14,5 +14,14 @@ def generate_invitations(template, attendees):
         return
     for attendee in attendees:
         if not isinstance(attendee, dict):
-            print("Error: Each element of attendees must be a dictionary.")
+            print("Error: Each element of attendees must be a dictionary")
             return
+        
+    if template.strip() == "":
+        print("Template is empty, no output files generated")
+        return
+    if len(attendees) == 0:
+        print("No data provided, no output files generated")
+        return
+    
+    placeholders = ["name", "event_title", "even_date", "event_location"]
