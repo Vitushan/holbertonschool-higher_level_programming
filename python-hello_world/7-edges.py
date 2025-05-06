554c698ca2220c34e3b8170b71ea9ae3467be14f
Vitushan <vitushan@outlook.fr>
2025-05-06 11:28:18 +0200

    slice 2.0

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(word[2:0])
+print(word[:9])
+print(word[1:9])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
