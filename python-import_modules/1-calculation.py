b0186bd0d2cd666cc3edf44ed666d887c0b88b79
Vitushan <vitushan@outlook.fr>
2025-05-09 14:48:38 +0200

    import modules

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(add(a, b)))
+    print("{} + {} = {}".format(sub(a, b)))
+    print("{} + {} = {}".format(mul(a, b)))
+    print("{} + {} = {}".format(div(a, b)))
