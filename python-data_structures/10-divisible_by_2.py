61a62db2cfff2e44e167ad7fa7ec7b4726df710e
Vitushan <vitushan@outlook.fr>
2025-05-12 14:37:58 +0200

    tuples

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            return True
+        else:
+            return False
