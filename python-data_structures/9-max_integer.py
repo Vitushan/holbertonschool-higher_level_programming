bee88abc77f35ad8fe3b96634126997d7ea512cd
Vitushan <vitushan@outlook.fr>
2025-05-12 14:23:23 +0200

    tuples

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,4 +2,8 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    return my_list
+    bigger_value = my_list[0]
+    for i in my_list:
+        if i > bigger_value:
+            bigger_value = i
+    return bigger_value
