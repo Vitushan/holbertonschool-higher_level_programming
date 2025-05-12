fcd564d4ef31a99878071c74eff70663ea75d5e9
Vitushan <vitushan@outlook.fr>
2025-05-12 14:48:52 +0200

    tuples

--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return new_list
+
