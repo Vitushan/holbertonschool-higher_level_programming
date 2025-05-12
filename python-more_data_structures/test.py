6dbbcc6a979973660eeba5774097bd3153cfd723
Vitushan <vitushan@outlook.fr>
2025-05-12 16:26:01 +0200

    set

--- python-more_data_structures/test.py
+++ python-more_data_structures/test.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    return sum(set(my_list))
+def print_sorted_dictionary(a_dictionary):
+    for key in sorted(a_dictionary.keys()):
+        print(f"{key}: {a_dictionary[key]}")
