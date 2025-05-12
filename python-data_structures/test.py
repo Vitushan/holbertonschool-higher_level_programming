74a301a0dd0ab4015284ae7ad2447a8f97d688cb
Vitushan <vitushan@outlook.fr>
2025-05-12 14:55:34 +0200

    tuples

--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for num in my_list:
-        if num % 2 == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return new_list
-
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        del my_list[idx]
+        return my_list
