fdcf05e2683c69ea9a0f33e4cdb6ac478bdd19d8
Vitushan <vitushan@outlook.fr>
2025-05-12 12:44:51 +0200

    list

--- python-data_structures/test.py
+++ python-data_structures/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    new_list = my_list.copy()
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
