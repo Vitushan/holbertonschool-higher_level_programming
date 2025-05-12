72b412e116e94b1deee657ade2a25c5b03c0b832
Vitushan <vitushan@outlook.fr>
2025-05-12 16:10:56 +0200

    set

--- python-more_data_structures/test.py
+++ python-more_data_structures/test.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_list = []
-    for item in my_list:
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-    return new_list
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
