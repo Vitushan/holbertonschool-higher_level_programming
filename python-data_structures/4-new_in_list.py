03776f7d4a626c854319bae3c065d9f23971b8bf
Vitushan <vitushan@outlook.fr>
2025-05-12 12:06:05 +0200

    list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 import copy
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        element += my_list.copy()
+        element = my_list.copy()
         return element
