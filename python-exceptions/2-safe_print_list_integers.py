f673f5b9557192d7480594d6ae4d726294cdbc06
Vitushan <vitushan@outlook.fr>
2025-05-12 20:55:27 +0200

    try except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             if isinstance(my_list[i], int):
-                count += 1
                 print("{:d}".format(my_list[i], end=""))
+                count += 1
         except IndexError:
             break
     print()
