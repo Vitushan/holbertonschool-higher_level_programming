56a8431fb783c6998f8544b516a39c636518ff14
Vitushan <vitushan@outlook.fr>
2025-05-12 12:32:08 +0200

    list

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = my_string
-    for i in new_string:
-        print("{}".format(i))
+    new_string = ""
+    for new_string in my_string:
+        if new_string != 'c' and new_string != 'C':
+            return new_string
+        return my_string
