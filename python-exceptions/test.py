5a9759af24da40f2f7e7c8a7bd303bb8b6662eb9
Vitushan <vitushan@outlook.fr>
2025-05-12 21:20:10 +0200

    try except

--- python-exceptions/test.py
+++ python-exceptions/test.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-        except IndexError:
-            break
-    print()
-    return count
+def safe_print_modulo(a, b):
+    try:
+        result = a % b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
