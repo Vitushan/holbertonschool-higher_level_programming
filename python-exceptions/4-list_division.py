ae03f5a45aa2c785163c7058a6a181f27f4a9737
Vitushan <vitushan@outlook.fr>
2025-05-13 14:13:29 +0200

    import

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -20,6 +20,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             result.append(0)
         finally:
-            pass
+            continue
 
     return result
