aa3b766ba1498e6bb42a7ea6b2fdbabeb5b1f712
Vitushan <vitushan@outlook.fr>
2025-05-13 14:04:16 +0200

    finnaly

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(a / b)
         except ZeroDivisionError:
             print("division by 0")
+            result.append(0)
         except IndexError:
             print("out of range")
             result.append(0)
