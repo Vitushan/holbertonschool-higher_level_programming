37e6ddf9ad2e3741811a768ce70dd075454e89bf
Vitushan <vitushan@outlook.fr>
2025-05-14 10:33:20 +0200

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,6 +8,7 @@ def add_integer(a, b=98):
     a and b must be first casted to integers if they are float
     Returns: an integer: the addition of a and b
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
