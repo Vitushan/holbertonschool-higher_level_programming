2cbfba2922818fb6f6d05ae217694c0f7f00542a
Vitushan <vitushan@outlook.fr>
2025-05-14 10:54:14 +0200

    test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by div.
+    Matrix must be a list of lists of ints/floats.
+    Each row must be the same size.
+    div must be a number and not zero.
+    Returns a new matrix with results rounded to 2 decimals.
+    """
     
