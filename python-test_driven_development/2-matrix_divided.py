8dabfc8553e499ccdca7a95efb2d9bf7e27743cd
Vitushan <vitushan@outlook.fr>
2025-05-15 14:01:53 +0200

    test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,25 +5,17 @@ def matrix_divided(matrix, div):
     Matrix must be a list of lists of ints/floats.
     Each row must be the same size.
     div must be a number and not zero.
-    Returns: a new matrix with results rounded to 2 decimals.
+    Returns a new matrix with results rounded to 2 decimals.
     """
- 
     if (not isinstance(matrix, list) or
         not all(isinstance(row, list) for row in matrix) or
         not all(isinstance(num, (int, float)) for row in matrix for num in row)):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
+    if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-
- 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
     new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
-
     return new_matrix
