8b91cc20d6f4ff0a3a5b86df635b3e33b5bee6b5
Vitushan <vitushan@outlook.fr>
2025-05-16 10:40:48 +0200

    import

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """
-she is a shebang for interpreting python3
+this a shebang for interpreting python3
 """
+
+
 def add_integer(a, b=98):
     """
-    Adds two integers.
-
-    >>> add_integer(1, 2)
-    3
-    >>> add_integer(100, -2)
-    98
-    >>> add_integer(2)
-    100
-    >>> add_integer(100.3, -2)
-    98
-    >>> add_integer(4, "School")
-    Traceback (most recent call last):
-        ...
-    TypeError: b must be an integer
-    >>> add_integer(None)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer
+    args: a and b must be integers or floats,
+    raise: otherwise raise a TypeError exception with
+    the message a must be an integer
+    or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns: an integer: the addition of a and b
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
