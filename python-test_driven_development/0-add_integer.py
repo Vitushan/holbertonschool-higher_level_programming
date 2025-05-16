38b96eb3e8d321dac7ed697c2ab2940078d045dc
Vitushan <vitushan@outlook.fr>
2025-05-16 10:50:11 +0200

    import

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 """
-This module provides a function to add two integers.
-
-It ensures that both arguments are integers or floats,
-casts floats to integers before addition,
-and raises appropriate errors for invalid inputs.
-
-The function is designed for educational purposes
-following the TDD (Test Driven Development) approach.
+this a shebang for interpreting python3
 """
-
 
 def add_integer(a, b=98):
     """
-    Adds two integers or floats.
-
-    Both arguments are casted to integers if they are floats.
-    Raises a TypeError if the arguments are not integers or floats.
+    args: a and b must be integers or floats,
+    raise: otherwise raise a TypeError exception with
+    the message a must be an integer
+    or b must be an integer
+    a and b must be first casted to integers if they are float
+    Returns: an integer: the addition of a and b
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
