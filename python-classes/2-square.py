d6fc7148b0fa64a1b47133fba41446f16e962050
Vitushan <vitushan@outlook.fr>
2025-05-19 00:51:51 +0200

    square class

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,3 +2,15 @@
 """
 module shebang for interpreting python3
 """
+
+
+class Square:
+    """
+    This is a Square class
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
