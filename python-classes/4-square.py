11f4cda0ae68f0efeb3311c9e60224e4bc551d49
Vitushan <vitushan@outlook.fr>
2025-05-19 10:12:56 +0200

    square class

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,7 +21,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         value = self.__size
-        return value
 
     def area(self):
         return self.__size * self.__size
