25d98642d8353c81b3a04ba9e677a3ec928a2f50
Vitushan <vitushan@outlook.fr>
2025-05-21 15:33:10 +0200

    pycodestyle

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,4 +52,4 @@ class Square:
             print()
 
         for _ in range(self.__size):
-            print(self.__position[0] * " " + self.__size * '#')
+            print(" " * self.__position[0] + "#" * self.__size)
