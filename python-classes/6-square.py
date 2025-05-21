d429c0c0e8fe24af7610050c3b295e3eb245fb5b
Vitushan <vitushan@outlook.fr>
2025-05-21 14:44:50 +0200

    last task

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -45,6 +45,8 @@ class Square:
         if self.__size == 0:
             print()
 
-        for i in range(self.__position[1]):
-            print(" " * "#" * self.__size)
+        for _ in range(self.__position[1]):
+            print()
 
+        for _ in range(self.__size):
+            print(self.__position[0] * " " + self.__size * '#')
