e4054cdb93f17608db65e1bd463b3599b8f50ca5
Vitushan <vitushan@outlook.fr>
2025-05-26 16:11:10 +0200

    geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is a shebang for interpreting python3
+"""
+
+
+class BaseGeometry:
+    """
+    this is a base geometry class
+    """
+    def area(self):
+        if isinstance(self.area, self.area()):
+            raise Exception("area() is not implemented")
