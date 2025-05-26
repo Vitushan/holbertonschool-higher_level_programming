5836d80ea634947587538f6c6a328a9b95a73177
Vitushan <vitushan@outlook.fr>
2025-05-26 11:26:05 +0200

    task0

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,5 +9,7 @@ class MyList(list):
     this is a list class sorted
     """
     def print_sorted(self):
-        self.print_sorted()
+        """
+        salut bonjour merde
+        """
         print(sorted(self))
