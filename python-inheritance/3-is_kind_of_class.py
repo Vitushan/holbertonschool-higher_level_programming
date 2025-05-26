c7488b766860371b6c0413be501a7115b85b2d67
Vitushan <vitushan@outlook.fr>
2025-05-26 14:55:00 +0200

    task3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -3,9 +3,10 @@
 this is a module for interpreting python3
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
-    this is a function
+    this is a functions
     """
     if isinstance(obj, a_class):
         return True
