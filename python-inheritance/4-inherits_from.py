6224532e10eb482f6541fdd8a45ed1bf4b54bbdd
Vitushan <vitushan@outlook.fr>
2025-05-26 15:09:19 +0200

    task3

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,4 +5,9 @@ this is a module for interpreting python3
 
 
 def inherits_from(obj, a_class):
-    
+    """
+    this is a inheritance class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
