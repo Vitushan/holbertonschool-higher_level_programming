d6d69688c36b8a1cec607cf20783575dc1a1faa9
Vitushan <vitushan@outlook.fr>
2025-05-26 19:18:39 +0200

    geometry

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -16,4 +16,4 @@ class Square(Rectangle):
         self.integer_validator("size", size)
 
     def area(self):
-        self.__width * self.__height
+        return (self.__width + self.__height) * 2
