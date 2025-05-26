4304a255e5feaad3933f002a2b54f83e135d06ff
Vitushan <vitushan@outlook.fr>
2025-05-26 11:41:33 +0200

    task0

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,8 +10,7 @@ class MyList(list):
     """
     def print_sorted(self):
         """
-        Prints the list in ascending sorted order (without modifying the original list).
+        Prints the list in ascending sorted order.
         Assumes all elements are integers.
         """
-        for _ in range(list):
-            print(sorted(self))
+        print(sorted(list))
