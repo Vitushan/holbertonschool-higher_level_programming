fc369b6c6c1aed44baff720e68bfc58ef24b778d
Vitushan <vitushan@outlook.fr>
2025-05-26 18:14:14 +0200

    geometry

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,6 +14,11 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
+        self.integer_validator = ('width', width)
+        self.integer_validator = ('height', height)
 
     def area(self):
         raise Exception("area() is not implemented")
+
+    def __str__(self):
+        return f"Rectangle: {self.__width}, {self.__height}"
