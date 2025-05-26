357363f4ed659842e67925d8a76226a06d25de70
Vitushan <vitushan@outlook.fr>
2025-05-26 10:45:54 +0200

    task0

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,7 @@ Module that defines print sorted.
 
 class MyList(list):
     """
-    this is a list class
+    this is a list class sorted
     """
     def print_sorted(self):
         print(sorted(self))
