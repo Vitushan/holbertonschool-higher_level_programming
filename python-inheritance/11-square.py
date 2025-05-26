be0b9a1df5c7286534a336a7ce888e59c00f3741
Vitushan <vitushan@outlook.fr>
2025-05-26 19:37:30 +0200

    geometry

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """
+    this is a class inheritance from Rectangle
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator('size', size)
+
+    def area(self):
+        return self.__size * self.__size
