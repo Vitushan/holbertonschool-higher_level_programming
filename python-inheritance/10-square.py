835e93cf081b173c26a8265051a373b13e0ccfee
Vitushan <vitushan@outlook.fr>
2025-05-26 19:10:54 +0200

    geometry

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,13 +6,13 @@ this is a module for interpreting python3
 
 BaseGeometry = __import__('9-rectangle').BaseGeometry
 
-
-def __init__(self, size):
+class Square:
     """
-    this is a size init
+    this is a square class
     """
-    self.__size = size
-    self.integer_validator("size", self.__size)
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
 
-def area(self):
-    self.__width * self.__height
+    def area(self):
+        self.__width * self.__height
