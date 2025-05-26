3cfeb9152d68c10471154f92fa0af4d1460d2692
Vitushan <vitushan@outlook.fr>
2025-05-26 15:16:19 +0200

    task3

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
     """
     this is a inheritance class
     """
-    if isinstance(obj, a_class):
+    if all(isinstance(obj, a_class)):
         return True
     return False
