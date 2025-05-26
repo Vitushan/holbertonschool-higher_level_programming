7ccd69a13e8aa97d52441aaf8e1d3d9cc1ce3aa2
Vitushan <vitushan@outlook.fr>
2025-05-26 12:16:18 +0200

    task1

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,6 +5,9 @@ Module that defines True is the object is instance
 
 
 def is_same_class(obj, a_class):
+    """
+    Is same class
+    """
     if type(obj) == a_class:
         return True
     else:
