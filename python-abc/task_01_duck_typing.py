57c445d29e8558992e8273fe6205dfa735a9db84
Vitushan <vitushan@outlook.fr>
2025-05-27 14:52:44 +0200

    radius

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,3 +2,46 @@
 """
 This is a module for interpreting python3
 """
+from abc import ABC, abstractmethod
+from math import *
+
+class Shape(ABC):
+    """
+    this is a shape class with ABC inheritance
+    """
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    """
+    this is a circle class inheritance from Shape class
+    """
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * pi
+
+    def perimeter(self):
+        return 2 * self.radius * pi
+
+class Rectangle(Shape):
+    """
+    this is a rectangle class
+    """
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        return (self.width + self.height) * 2
+
+if __name__ == '__main__':
