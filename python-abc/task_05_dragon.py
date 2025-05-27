145837349cd060826e6cdd6868ab7cf9b9649c02
Vitushan <vitushan@outlook.fr>
2025-05-27 19:10:52 +0200

    multiple inheritance

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -2,3 +2,21 @@
 """
 This is a module for interpreting python3
 """
+
+
+class SwimMixin:
+    """
+    this is a mixin class
+    """
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
