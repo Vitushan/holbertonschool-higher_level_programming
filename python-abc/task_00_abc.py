16622c2bda02bc4aa029fd233ac7d785c0215ba4
Vitushan <vitushan@outlook.fr>
2025-05-27 13:51:13 +0200

    abc method

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -2,3 +2,20 @@
 """
 This is a module for interpreting python3
 """
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    this is a Animal class inheritance ABC
+    """
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return f"Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return f"Meow"
