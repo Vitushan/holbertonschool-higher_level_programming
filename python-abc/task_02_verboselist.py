910b3a3baf4e1ca34f633bfd0e00abae6283d979
Vitushan <vitushan@outlook.fr>
2025-05-27 19:00:04 +0200

    multiple inheritance

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -2,3 +2,7 @@
 """
 This is a module for interpreting python3
 """
+class VerboseList(list):
+    """
+    this is a class
+    """
--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -2,3 +2,38 @@
 """
 This is a module for interpreting python3
 """
+
+
+class Fish:
+    """
+    this is a Fish class
+    """
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitat(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """
+    This is a Bird class
+    """
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+class FlyingFish(Fish, Bird):
+    """
+    this is a multiple inheritance class
+    """
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
