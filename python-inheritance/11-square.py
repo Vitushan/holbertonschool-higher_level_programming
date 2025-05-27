03823353851eefceb2022100920e4559ab987382
Vitushan <vitushan@outlook.fr>
2025-05-27 10:56:26 +0200

    task final

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -14,7 +14,6 @@ class Square(Rectangle):
     def __init__(self, size):
         self.__size = size
         self.integer_validator('size', size)
-        super().__init__(self, size)
 
     def area(self):
         return self.__size * self.__size
