5fe1a305e6a80cc7cfdee1682dc549755f04cd21
Vitushan <vitushan@outlook.fr>
2025-05-28 09:48:18 +0200

    mro

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -38,3 +38,7 @@ class FlyingFish(Fish, Bird):
 
     def habitat(self):
         print("The flying fish lives both in water and the sky!")
+
+    print(fly.mro())
+    print(swim.mro())
+    print(habitat.mro())
