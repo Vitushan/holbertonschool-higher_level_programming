bab25ba557d06db1a4b21e8e09f90746ce4745cc
Vitushan <vitushan@outlook.fr>
2025-05-28 10:43:29 +0200

    reparing tasks

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,3 +2,23 @@
 """
 This is a module for interpreting python3
 """
+
+
+class CountedIterator:
+    """
+    This is a countedIterator class
+    """
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise
+
+    def get_count(self):
+        return self.count
