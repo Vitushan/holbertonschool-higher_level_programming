e064e116425a56dcad9170e436161952ab31941e
Vitushan <vitushan@outlook.fr>
2025-06-02 09:52:48 +0200

    read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    readfile encoding utf8
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
