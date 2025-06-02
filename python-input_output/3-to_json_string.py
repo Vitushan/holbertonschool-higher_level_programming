79f80547c6a054b0f48b595bb069a85fe87de232
Vitushan <vitushan@outlook.fr>
2025-06-02 10:39:46 +0200

    read file

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    JSON representation of an object (string)
+    """
+    json.dumps(my_obj)
