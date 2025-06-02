ac100720a75624e3a38f78e7f76b480f771fe8da
Vitushan <vitushan@outlook.fr>
2025-06-02 10:27:33 +0200

    read file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    append write
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.append(text)
