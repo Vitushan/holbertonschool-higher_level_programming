3c4d9ae878b5c9873c47cc3176c04b1e5e8145b0
Vitushan <vitushan@outlook.fr>
2025-06-02 13:50:43 +0200

    load filename

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    load from json file
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.load(filename)
