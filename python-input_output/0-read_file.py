a6f017d822fa62f5f99b92930ab6b4329f0ed4f5
Vitushan <vitushan@outlook.fr>
2025-06-02 10:01:45 +0200

    read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -7,6 +7,5 @@ def read_file(filename=""):
     """
     readfile encoding utf8
     """
-    with open(filename, encoding="utf-8", end="") as f:
-        print(filename)
-        f.closed
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
