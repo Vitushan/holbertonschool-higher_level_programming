a7cf39824a2ea2d3022da192a259259cdcf8c889
Vitushan <vitushan@outlook.fr>
2025-06-02 11:42:19 +0200

    read file

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -14,4 +14,4 @@ def save_to_json_file(my_obj, filename):
      using a JSON representation
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, filename, f)
+        json.dump(my_obj, f)
