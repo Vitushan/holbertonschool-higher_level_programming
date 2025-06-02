e6349c0986fc1c400f2ccf9ccbd132ad4e7ab5a0
Vitushan <vitushan@outlook.fr>
2025-06-02 10:33:04 +0200

    read file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -8,5 +8,5 @@ def append_write(filename="", text=""):
     """
     append write
     """
-    with open(filename, 'a + r', encoding='utf-8') as f:
+    with open(filename, 'a', encoding='utf-8') as f:
         return f.write(text)
