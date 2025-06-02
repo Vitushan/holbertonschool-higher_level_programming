ed209f14cb0f1acfc76fc7a0f63099137593f5fc
Vitushan <vitushan@outlook.fr>
2025-06-02 10:22:36 +0200

    read file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
     write file with encoding utf8
     """
     with open(filename, 'w', encoding='utf-8') as f:
-        return f.write()
+        return f.write(text)
