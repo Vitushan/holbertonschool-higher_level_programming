145e7d1b92f0d7f22f5ef7c42b69f6be3b3bff42
Vitushan <vitushan@outlook.fr>
2025-06-03 15:31:47 +0200

    import sys for argv

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -12,13 +12,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_to_json_file
 
 
-file = 'add_item.json'
+filename = 'add_item.json'
 
 try:
-    my_list = load_from_json_file(file)
+    my_list = load_from_json_file(filename)
 except Exception:
     my_list = []
 
 my_list += sys.argv[1:]
 
-save_to_json_file(my_list, file)
+save_to_json_file(my_list, filename)
