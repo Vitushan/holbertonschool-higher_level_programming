07d4813aaa0e39897415a17e82b9f1507a2510ae
Vitushan <vitushan@outlook.fr>
2025-06-03 15:34:05 +0200

    import sys for argv

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,13 @@ this is a module for interpreting python3
 
 
 import sys
-import json
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_to_json_file
 
 
-filename = 'add_item.json'
+filename = "add_item.json"
 
 try:
     my_list = load_from_json_file(filename)
