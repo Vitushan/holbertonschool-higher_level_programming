f581d053f44426cc524555736feffcd419d541ba
Vitushan <vitushan@outlook.fr>
2025-06-03 16:37:06 +0200

    __dict__

---
+++ python-input_output/11-student.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+class Student:
+    """
+    this is a student class
+    """
---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+this is a module for interpreting python3
+"""
+
+
+def pascal_triangle(n):
+    """
+    pascal triangle
+    """
