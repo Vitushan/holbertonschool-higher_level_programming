3a9fe6df6192ed80a6a2f89096413b2ce3604afa
Vitushan <vitushan@outlook.fr>
2025-06-04 14:21:59 +0200

    pickle

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -7,9 +7,9 @@ this is a module for interpreting python3
 import pickle, json
 
 def serialize_and_save_to_file(data, filename):
-    with open(data, filename, 'rb', encoding='utf-8') as f:
-        json.dump(f)
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
 
 def load_and_deserialize(filename):
-    with open(filename, 'rb', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         json.load(f)
