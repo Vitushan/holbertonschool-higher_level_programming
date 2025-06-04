17dc292fe0e6ad7cbd6c520e77f78c7036cf4418
Vitushan <vitushan@outlook.fr>
2025-06-04 15:18:55 +0200

    pickle

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -2,3 +2,18 @@
 """
 this is a module for interpreting python3
 """
+
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"{self.name}")
+        print(f"{self.age}")
+        print(f"{self.is_student}")
