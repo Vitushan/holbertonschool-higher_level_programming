a26e9bdbdc511515079b8dcc1b96c6576c8eaea9
Vitushan <vitushan@outlook.fr>
2025-06-04 18:30:18 +0200

    pickle

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,3 +2,19 @@
 """
 this is a module for interpreting python3
 """
+
+
+import json
+import csv
+
+
+def convert_csv_to_json(filename):
+    """
+    
+    convert csv file to json file name data.json
+    """
+    try:
+        with open(filename.csv, 'r') as csvfile:
+            csv.DictReader(csvfile)
+    except Exception:
+        False
