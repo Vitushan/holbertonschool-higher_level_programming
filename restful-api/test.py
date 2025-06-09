f2af0ad0671dc39b436ea73ca793ee4e90965c9d
Vitushan <vitushan@outlook.fr>
2025-06-09 13:19:11 +0200

    requests

--- restful-api/test.py
+++ restful-api/test.py
@@ -12,7 +12,7 @@ def fetch_and_print_posts():
     fetches all post from JSONPlaceholder.
     """
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    print(f"Status Code: {response.status_code}")
+    print(f"Status code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
