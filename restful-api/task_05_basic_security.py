649aa4097ef95904f4d905bc756e5817ee550408
Vitushan <vitushan@outlook.fr>
2025-06-12 19:08:51 +0200

    fixing task

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -4,47 +4,68 @@ this is a module for interpreting python3
 """
 
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import get_jwt_identity
 
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
-
-
-
+app.config["JWT_SECRET_KEY"] = "super-secret"
+jwt = JWTManager(app)
 
 users = {
     "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
     "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
 }
 
-
-
 @auth.verify_password
 def verify_password(username, password):
-    if username in users and \
-            check_password_hash(users.get(username), password):
+    user = users.get(username)
+    if user and check_password_hash(user["password"], password):
         return username
+    return None
 
-@app.route('/login')
+
+@app.route('/basic-protected')
 @auth.login_required
+def basic_protected():
+      return "Basic Auth: Access Granted"
+
+
+
+
+@app.route('/login', methods=['POST'])
 def login():
-    return "Hello, {}!".format(auth.current_user())
+    username = request.json.get("username")
+    password = request.json.get("password")
+    if not username or not password:
+        return jsonify({"error": "Username and password required"}), 400
+    user = users.get(username)
+    if user and check_password_hash(user["password"], password):
+        access_token = create_access_token(identity={
+            "username": username,
+            "role": user["role"]
+        })
+        return jsonify(access_token=access_token)
+    return jsonify({"error": "Invalid credentials"}), 401
 
-@auth.get_user_roles
-def get_user_roles(user):
-    return user.get_roles()
 
-@app.route('/admin')
-@auth.login_required(role='admin')
+@app.route('/jwt-protected')
+@jwt_required()
+def jwt_protected():
+    return "JWT Auth: Access"
+    
+
+@app.route('/admin-only', methods=['GET'])
+@jwt_required()
 def admin_only():
-    return "Hello {}, you are an admin!".format(auth.current_user())
+ users = get_jwt_identity()
 
-@auth.error_handler
-def auth_error(status):
-    return "Access Denied", status
-
-if __name__ == "__main__":
-    app.run()
+ if users and users.get('role') == "admin":
+    return "Admin Access: Granted"
+ return jsonify( {"error": "Admin access required"}), 403
