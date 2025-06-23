7a6a12620dd9729600fa5a20618270542ad4d07f
Vitushan <vitushan@outlook.fr>
2025-06-23 16:30:43 +0200

    avg

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,5 @@
 -- Script that computes the score average of all records in the second table
+-- Change name by alias 'as' (score -> average) 
 
 
 SELECT AVG(score) as average FROM second_table;
--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,4 @@
 -- script that lists the number of records with the same score in the second table
 
 
+SELECT score FROM second_table GROUP BY score DESC;
