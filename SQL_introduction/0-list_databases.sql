cbbecd39e3d33d056bc079a78414af1b1b49d609
Vitushan <vitushan@outlook.fr>
2025-06-23 10:39:05 +0200

    SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -5,23 +5,23 @@ This script connects to a MySQL server and lists all databases.
 
 import mysql.connector
 
-# Connexion au serveur MySQL
+
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password=""
+    host:"localhost",
+    user:"root",
+    password:""
 )
 
 cursor = conn.cursor()
 
-# Envoi de la commande SQL SHOW DATABASES
+
 cursor.execute("SHOW DATABASES;")
 
-# Affichage des résultats
+
 print("Databases on this MySQL server:")
 for db in cursor:
     print(f"- {db[0]}")
 
-# Fermeture
+
 cursor.close()
 conn.close()
