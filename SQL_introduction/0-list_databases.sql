74913649fff8f85b831e1ded2ac928627254398e
Vitushan <vitushan@outlook.fr>
2025-06-23 10:24:17 +0200

    SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -4,12 +4,4 @@ This is a module for interpreting python3
 """
 
 
-import mysql.connector as MC
-
-
-conn = Mc.connect(
-    host='localhost',
-    user='root',
-    password='',
-)
-cursor = conn.cursor()
+SHOW DATABASES;
