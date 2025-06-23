25311dd91e5741f2b0c45d577f96e555453958a8
Vitushan <vitushan@outlook.fr>
2025-06-23 10:14:12 +0200

    SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -4,4 +4,15 @@ This is a module for interpreting python3
 """
 
 
-SHOW DATABASES *
+from mysql.connector as MC
+
+try:
+    conn = MC.connect(host = 'localhost', database = 'data', user = 'root', password= '')
+    cursor = conn.cursor()
+ 
+except MC.Error as err:
+    print(err)
+finally:
+    if(conn.is_connected()):
+        cursor.close()
+        conn.close
