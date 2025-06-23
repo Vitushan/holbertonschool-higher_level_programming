b2c28710ab68a6395fd9e5df4b902908d45a0c66
Vitushan <vitushan@outlook.fr>
2025-06-23 10:17:13 +0200

    SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -4,15 +4,9 @@ This is a module for interpreting python3
 """
 
 
-from mysql.connector as MC
+import mysql.connector as MC
 
-try:
+
     conn = MC.connect(host = 'localhost', database = 'data', user = 'root', password= '')
     cursor = conn.cursor()
  
-except MC.Error as err:
-    print(err)
-finally:
-    if(conn.is_connected()):
-        cursor.close()
-        conn.close
