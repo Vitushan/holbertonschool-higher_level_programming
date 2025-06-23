1c90402ad180b24790b3416fb471f26db2f47fb5
Vitushan <vitushan@outlook.fr>
2025-06-23 16:36:18 +0200

    avg

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- script that lists the number of records with the same score in the second table
 
 
-SELECT COUNT(*) score as number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT COUNT(*), score as number FROM second_table GROUP BY score ORDER BY number DESC;
