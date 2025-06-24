d15100b2c09f532bb8c4ba99231e7c8507f570df
Vitushan <vitushan@outlook.fr>
2025-06-24 11:45:29 +0200

    states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,3 +2,8 @@
 -- and the table states (in the database hbtn_0d_usa)
 
 
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `states` (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT AND PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
