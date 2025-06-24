c85272af1752ceeca0edbc3d08b619f39adff1f3
Vitushan <vitushan@outlook.fr>
2025-06-24 10:00:14 +0200

    privilege

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 
 
-GRANTS ALL PRIVILEGES ON 'user_0d_1'@'localhost'
-GRANTS ALL PRIVILEGES ON 'user_0d_2'@'localhost'
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+GRANTS ALL PRIVILEGES ON *.* TO'user_0d_2'@'localhost'
