675e87742ef04275abaa60e5317de0232c4ec77a
Vitushan <vitushan@outlook.fr>
2025-06-24 13:06:57 +0200

    states_ID

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS `cities` (
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
-    FOREIGN KEY(id) REFERENCES states_id(id)
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
