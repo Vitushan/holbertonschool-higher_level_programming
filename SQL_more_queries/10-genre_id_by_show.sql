0e0356e793855a14e7b06a3e1ba0c1cf0e227491
Vitushan <vitushan@outlook.fr>
2025-06-25 11:38:42 +0200

    JOIN INNER

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -2,3 +2,6 @@
 -- hbtn_0d_tvshows that have at least one genre linked.
 
 
+SELECT tv_shows.title, tv_shows_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_shows ON tv_shows.title = tv_shows_genres_id.genre_id
