9d74e58bf005334855af450c5ba9377bf561f9f0
Vitushan <vitushan@outlook.fr>
2025-06-25 11:44:11 +0200

    JOIN INNER

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,5 @@
 
 SELECT tv_shows.title, tv_shows_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_shows ON tv_shows.title = tv_shows_genres_id.genre_id;
+INNER JOIN tv_show_genres ON tv_shows.id = tv_shows_genres.show_id
+ORDER BY tv_shows.title AND tv_show_genres.genre_id
