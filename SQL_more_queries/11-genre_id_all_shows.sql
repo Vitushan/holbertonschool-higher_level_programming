3f5e09983ba01e4e5439f2eb149b2bbc63a46a29
Vitushan <vitushan@outlook.fr>
2025-06-25 13:58:01 +0200

    JOIN INNER

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,9 @@
 -- script that lists all shows contained
 -- in the database hbtn_0d_tvshows
+
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+WHERE = tv_show_genres NOT NULL
+INNER JOIN tv_shows ON tv_show_genres = tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC;
