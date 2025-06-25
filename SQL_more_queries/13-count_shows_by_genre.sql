5a78d3a8d41c4da9679a1af307e343fa2b5cfbad
Vitushan <vitushan@outlook.fr>
2025-06-25 15:54:22 +0200

    LEFT JOIN

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,3 +3,8 @@
 -- the number of shows linked to each.
 
 
+SELECT tv_genres AS genre, COUNT(tv_show_genres) AS number_of_shows
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
