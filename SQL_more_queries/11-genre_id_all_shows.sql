fca17be5015ea673a2a5e9df6d04fcc0d908a911
Vitushan <vitushan@outlook.fr>
2025-06-25 14:05:16 +0200

    JOIN INNER

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,5 +5,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 WHERE = tv_show_genres NOT NULL
-INNER JOIN tv_shows ON tv_show_genres = tv_show_genres.genre_id
-ORDER BY tv_shows.title ASC;
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
