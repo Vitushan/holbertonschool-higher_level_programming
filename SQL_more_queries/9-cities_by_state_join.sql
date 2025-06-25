2ff4f85bd6fb1b8ddd2ae9c0e6050509ceb5ba84
Vitushan <vitushan@outlook.fr>
2025-06-25 10:24:07 +0200

    california

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- script that lists all cities contained in the database hbtn_0d_usa
 
 
-SELECT cities.id, cities.name, states.name
+SELECT cities_id, cities_name, states_name
 FROM cities
 INNER JOIN states ON cities.states_id = state.id
 ORDER BY cities.id ASC
