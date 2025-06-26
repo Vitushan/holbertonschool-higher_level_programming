80e20b4da9c22e818fe7a45068cee014e2127452
Vitushan <vitushan@outlook.fr>
2025-06-26 20:48:01 +0200

    add contraint binary

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states " \
-        "WHERE name LIKE BINARY 'N%' " \
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY 'N%' "
         "ORDER BY id ASC"
     )
 
