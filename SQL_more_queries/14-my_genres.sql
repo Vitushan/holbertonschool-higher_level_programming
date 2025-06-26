dac2b9e2a44914223bee73301b7842cde6575619
Vitushan <vitushan@outlook.fr>
2025-06-26 14:53:03 +0200

    INNER JOIN

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,3 +2,9 @@
 -- database to lists all genres of the show Dexter
 
 
+SELECT tv_genres.name
+FROM tv_genre
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
