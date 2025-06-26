2d531a2f21e7da12e63fe4f9246b2536f899ca6f
Vitushan <vitushan@outlook.fr>
2025-06-26 22:05:00 +0200

    use format and query

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -3,3 +3,24 @@ script that lists all cities from the database hbtn_0e_4_usa
 """
 
 
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
+    cursor = db.cursor()
+
+    query = "SELECT * FROM cities ORDER BY id ASC"
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
