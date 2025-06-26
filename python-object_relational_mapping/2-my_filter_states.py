07c4c0324775fcea992e1fac2f28c9a4f97a519b
Vitushan <vitushan@outlook.fr>
2025-06-26 21:28:12 +0200

    use format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,12 @@
+#!/usr/bin/python3
 """
-script that takes in an argument and
+Script that takes in an argument and
 displays all values in the states table
 of hbtn_0e_0_usa where name matches the argument.
 """
 
-
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
@@ -15,15 +14,19 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name_searched = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306, user=username,
-                         passwd=password, db=db_name)
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=db_name)
 
     cursor = db.cursor()
 
-    cursor.execute(
-        "SELECT * FROM states "
-        "WHERE name = '{}' "
-        "ORDER BY states.id ASC".format(state_name_searched))
+    query = ("SELECT * FROM states "
+             "WHERE name = '{}' "
+             "ORDER BY states.id ASC").format(state_name_searched.replace("'", "''"))
+    cursor.execute(query)
 
     rows = cursor.fetchall()
     for row in rows:
