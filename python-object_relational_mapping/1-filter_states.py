3bdcf109d10a443bfaa64bcfd4ed251eb60223b1
Vitushan <vitushan@outlook.fr>
2025-06-26 20:31:02 +0200

    N%

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,26 +8,20 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Arguments du terminal
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connexion à MySQL
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
                          passwd=password, db=db_name)
 
-    # Curseur pour exécuter les requêtes
     cursor = db.cursor()
 
-    # Requête : noms commençant par 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Affiche les résultats
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
-    # Fermeture
     cursor.close()
     db.close()
