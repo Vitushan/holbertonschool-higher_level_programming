9182b4467c725d71149474d40d77853508aab9c2
Vitushan <vitushan@outlook.fr>
2025-06-26 15:23:20 +0200

    INNER JOIN

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -4,6 +4,6 @@
 SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
