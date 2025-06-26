40d8327475e6a9f86a5ac6784be1e0b55e50cbb6
Vitushan <vitushan@outlook.fr>
2025-06-26 21:17:26 +0200

    use format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -21,3 +21,10 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name_searched))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
