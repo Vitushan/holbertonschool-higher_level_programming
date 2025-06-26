43e2e089ab160df97bf2b017435863f7d7024564
Vitushan <vitushan@outlook.fr>
2025-06-26 21:13:02 +0200

    use format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,3 +5,19 @@ ofhbtn_0e_0_usa where name matches the argument.
 """
 
 
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=db_name)
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC".format(state_name_searched))
