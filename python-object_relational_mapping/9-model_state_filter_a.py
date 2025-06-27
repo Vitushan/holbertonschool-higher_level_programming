3e90350f47d025c75dd4dbe39238a7ede7b06289
Vitushan <vitushan@outlook.fr>
2025-06-27 19:17:40 +0200

    first()

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+script that lists all State objects that
+contain the letter a from the database hbtn_0e_6_usa
 """
 
 
@@ -23,7 +24,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    states_with_a = session.query(State).filter(
+        State.name.like('%a%')
+        ).order_by(State.id).all()
 
     for state in states_with_a:
         print(f"{state.id}: {state.name}")
