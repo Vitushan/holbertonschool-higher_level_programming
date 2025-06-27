512992f59945c54cb506b94422dd654435fa579a
Vitushan <vitushan@outlook.fr>
2025-06-27 19:45:37 +0200

    add and commit new state

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -4,3 +4,28 @@ script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
 
 
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
