58d67b5a172dfbe7f5f45d1003258fadfe91ac24
Vitushan <vitushan@outlook.fr>
2025-06-27 11:39:53 +0200

    task5

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
 """
--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Python file similar to model_state.py
 named model_city.py that contains the class definition of a City.
