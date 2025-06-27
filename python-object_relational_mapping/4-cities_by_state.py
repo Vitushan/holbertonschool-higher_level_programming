10cb25500fbb4b1dd6ccd037c1a3dd701015e7c2
Vitushan <vitushan@outlook.fr>
2025-06-27 11:25:03 +0200

    task5

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         FROM cities
         JOIN states ON cities.state_id = states.id
         ORDER BY cities.id ASC
-    """
+        """
     cursor.execute(query)
 
     rows = cursor.fetchall()
--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -3,3 +3,37 @@ script that takes in the name of a state as an argument and lists all cities of 
 """
 
 
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        db=db_name,
+        state_name=state_name
+    )
+    cursor = db.cursor()
+
+    query = """
+        SELECT cities.id, cities.name, states.name
+        FROM cities
+        JOIN states ON cities.name, states.name
+        ORDER BY cities.id ASC
+"""
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
