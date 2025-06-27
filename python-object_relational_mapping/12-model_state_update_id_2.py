92c31aa7297170b686b4d878e08c134bcb8325a7
Vitushan <vitushan@outlook.fr>
2025-06-27 19:54:34 +0200

    filter by ID

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -4,3 +4,31 @@ script that changes the name
 of a State object from the database hbtn_0e_6_usa
 """
 
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+         f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
+         pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_name).first()
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()
