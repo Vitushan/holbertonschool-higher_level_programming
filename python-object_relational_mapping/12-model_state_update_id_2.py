51c229b64e8d1611bdf52cdbc0011836b8dadb73
Vitushan <vitushan@outlook.fr>
2025-06-27 19:55:48 +0200

    filter by ID

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
     engine = create_engine(
          f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
