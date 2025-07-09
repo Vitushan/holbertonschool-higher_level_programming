d80ea0f17038be9b51fd9cc6dae26485d5addeca
Vitushan <vitushan@outlook.fr>
2025-07-09 19:34:09 +0200

    argv

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-    for (let line of lines) console.log(line);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const line of lines) console.log(line);
