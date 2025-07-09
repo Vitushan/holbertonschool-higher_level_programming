d92957263a5d8dcd277346fa3e5f5ee6fe322ac6
Vitushan <vitushan@outlook.fr>
2025-07-09 15:44:57 +0200

    args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 
+const { argv } = require('node:process');
 
-if (argv === 0) {
+
+if (argv.length === 0) {
     console.log("No argument");
-} else if(argv === 1) {
+} else if(argv.length === 1) {
     console.log("Argument found");
 } else {
     console.log("Arguments found");
