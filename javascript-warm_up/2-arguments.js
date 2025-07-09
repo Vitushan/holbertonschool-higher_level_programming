6ed3795c385c97d9042334dc5b43fb7f9604bc37
Vitushan <vitushan@outlook.fr>
2025-07-09 15:23:06 +0200

    args

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+
+if (args === 0) {
+    console.log("No argument");
+} else if(args === 1) {
+    console.log("Argument found");
+} else {
+    console.log("arguments found");
+}
