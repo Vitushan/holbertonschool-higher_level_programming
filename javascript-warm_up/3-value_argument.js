007a81355eca1a6c65023c445231fb4841b7f80b
Vitushan <vitushan@outlook.fr>
2025-07-09 19:55:32 +0200

    argv

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined) {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2]);
-}
+const { argv } = require('node:process');
 
+if (argv[2] === undefined) {
+    console.log('No argument');
+} else {
+    console.log(argv[2]);
+}
