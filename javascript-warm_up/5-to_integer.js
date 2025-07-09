26a1df9aea20d051e06cd617f6a906e3a20db511
Vitushan <vitushan@outlook.fr>
2025-07-09 16:06:08 +0200

    argv

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const { argv } = require('node:process');
+const num = Number(argv[2]);
+
+if (!isNaN(num)) {
+    console.log(`My number: ${num}`);
+}else {
+    console.log('Not a number');
+}
