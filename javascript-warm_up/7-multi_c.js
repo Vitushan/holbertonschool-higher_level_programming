be8a5eb68eba45240b7bbf6a70461d6407a9b6aa
Vitushan <vitushan@outlook.fr>
2025-07-10 13:53:14 +0200

    export model for function add

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 const { argv } = require('node:process')
 const x = Number(argv[2]);
 
-if (!isNaN(x)) {
+if (isNaN(x)) {
     console.log('Missing number of occurrences');
 }else {
     console.log(x);
