ecb4225fbc8d54b4c6d9896140d41e7dd479613c
Vitushan <vitushan@outlook.fr>
2025-07-10 13:45:20 +0200

    export model for function add

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const { args } = require('node:process');
+const { argv } = require('node:process');
 
-if (args.length <= 2) {
+if (argv.length <= 2) {
     console.log('No argument');
-} else if(args.length === 3) {
+} else if(argv.length === 3) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
