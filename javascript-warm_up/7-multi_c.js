58762f70bf2cf63a45f93a7abb1b93d551644ff1
Vitushan <vitushan@outlook.fr>
2025-07-10 13:56:27 +0200

    export model for function add

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ const x = Number(argv[2]);
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
 }else {
-    for(let i = 0; x < 5; i++);
+    for(let i = 0; i < 5; i++)
     console.log('C is fun');
 }
