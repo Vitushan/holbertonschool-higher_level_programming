3c86580b2a847aa16ca5f2ef7b254a4ac362b65c
Vitushan <vitushan@outlook.fr>
2025-07-10 13:57:36 +0200

    loop x times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ const x = Number(argv[2]);
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
 }else {
-    for(let i = 0; i < 5; i++)
+    for(let i = 0; i < x; i++)
     console.log('C is fun');
 }
