3cb083beb8c3615215005e1144ff8504574e571e
Vitushan <vitushan@outlook.fr>
2025-07-10 14:03:03 +0200

    loop x times

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process')
-const x = Number(argv[2]);
+const x = parseInt(argv[2]);
 
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
-}else {
+} else {
     for(let i = 0; i < x; i++) {
         console.log('C is fun');
     }
