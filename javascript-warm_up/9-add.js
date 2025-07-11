c4284c67cd4a36c4d1ced2541ff6c927ce85b90b
Vitushan <vitushan@outlook.fr>
2025-07-11 14:58:56 +0200

    fixing task

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
+const { argv } = require('node:process');
 function add(a, b) {
     return a + b
 }
 
-const { argv } = require('node:process');
 const a = Number(argv[2]);
 const b = Number(argv[3]);
 console.log(add(a, b));
