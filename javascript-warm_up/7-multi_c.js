b14af23576fb11ec5cc18e10469ab576b977e00d
Vitushan <vitushan@outlook.fr>
2025-07-11 09:34:16 +0200

    Task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,8 +5,8 @@ const x = Number(argv[2]);
 
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
-}else {
+} else {
     for(let i = 0; i < x; i++) {
-    console.log('C is fun');
+        console.log('C is fun');
     }
 }
