e0ea9f0c483f75bdce563679b63127dc051ff371
Vitushan <vitushan@outlook.fr>
2025-07-11 09:54:10 +0200

    Task7

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const { argv } = require('node:process')
-const x = Number(argv[2]);
+const x = parseInt(argv[2]);
 
 if (isNaN(x)) {
     console.log('Missing number of occurrences');
