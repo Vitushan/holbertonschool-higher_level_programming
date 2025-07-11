04b11389b0f3751db6fa3b193c3489d18b41be02
Vitushan <vitushan@outlook.fr>
2025-07-11 11:22:56 +0200

    fixing task

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-let n = 5, string = ""
+let X = 5, string = ""
     for (let i = 1; i < n; i++) {
     for (let j = 0; j < n; j++) {
         string += "*";
       }
-      //break
       string += "\n";
     }
     console.log(string);
-    
