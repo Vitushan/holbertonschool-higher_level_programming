2caa1ac2b2f5e26316781bd88f4271e13282a96d
Vitushan <vitushan@outlook.fr>
2025-07-14 18:00:52 +0200

    toggle_header

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -2,4 +2,8 @@ const toggleHeader = document.getElementById('toggle_header');
 
 toggleHeader.addEventListener('click', function () {
     const header = document.querySelector('header');
-})
+    if (header.classList.contains('red')) {
+        header.classList.remove('red');
+        header.classList.add('red');
+    }
+});
