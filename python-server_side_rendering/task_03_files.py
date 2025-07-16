3fa2ccea9a1820e5e958ee069e707176da9fb6a1
Vitushan <vitushan@outlook.fr>
2025-07-16 13:53:38 +0200

    parse json

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -5,3 +5,14 @@ import csv
 app = Flask(__name__)
 
 
+@app.route('/product_display')
+def product_display(force=False, silent=False):
+    with open('product_display', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return render_template('product_display.html', product_display=data.get('product_display'))
+
+    with open('product_display', 'r', encoding='utf-8'):
+        data = csv.dumps(f)
+        return render_template('product_display.html', product_display=data.get('product_display'))
+
+
