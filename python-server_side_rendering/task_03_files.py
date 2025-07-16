9a1a66ab8e7f316b72ab3575eaf753f949daed9e
Vitushan <vitushan@outlook.fr>
2025-07-16 14:50:21 +0200

    wrong source

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -18,11 +18,21 @@ def read_csv():
 @app.route('/product_display')
 def product_display():
     src = request.args.get('src')
+    product_id = request.args.get('id')
 
     if src == 'json':
-        return read_json()
+        products = read_json()
     elif src == 'csv':
-        return read_csv()
+        products = read_csv()
     else:
-        return render_template('product_display.html', error="Wrong source"), 404
+        return render_template('product_display.html', error="Wrong source")
  
+    if product_id:
+        select_products = []
+        for product in products:
+            if str(product.get('id')) == str(product_id):
+                select_products.append(product)
+        if not select_products:
+            return render_template('product_display.html', error="Product not found")
+        products = select_products
+    return render_template('product_display.html', products=products)
