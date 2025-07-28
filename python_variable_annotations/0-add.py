462ff9731eae857c20defaf00b3a69d069173245
Vitushan <vitushan@outlook.fr>
2025-07-28 13:30:46 +0200

    annotation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -6,6 +6,12 @@ This is a module for interpreting python3 s
 
 def add(a: float, b:float) -> float:
     """
-    Annotation float
+    Add two floats and returns their sum.
+
+    Args:
+    a (float: First number)
+    b (float): Second number
+
+    Return float: Sums of a and b
     """
     return a + b
